1168ebb779ba80a6357270fea948a8bd0ffe32f2
marc2332 <mespinsanz@gmail.com>
2022-08-22 18:56:16 +0200

    chore: updated shadow example

--- examples/shadow.rs
+++ examples/shadow.rs
@@ -1,4 +1,4 @@
-use dioxus::prelude::*;
+use dioxus::{prelude::*, core::UiEvent, events::MouseData};
 use elements_namespace as dioxus_elements;
 
 use trev::launch;
@@ -8,13 +8,24 @@ fn main() {
 }
 
 fn app(cx: Scope) -> Element {
+
+    let shadow_size = use_state(&cx, || (150,));
+
+    let onscroll = move |ev: UiEvent<MouseData>| {
+        shadow_size.with_mut(|shadow_size| {
+            let page = ev.coordinates().page();
+            shadow_size.0 += (page.y as i32) * 10;
+        })
+    };
+
     cx.render(rsx!(
         view {
             height: "100%",
             width: "100%",
-            padding: "50",
+            padding: "125",
+            onscroll: onscroll,
             view {
-                shadow: "0 5 200 50.0 red",
+                shadow: "0 10 {shadow_size.0} 80.0 red",
                 height: "100%",
                 width: "100%",
                 background: "black",
