8f82d1df6cf3f7df67f40430757b179c7b93c237
marc2332 <mespinsanz@gmail.com>
2022-11-05 14:14:09 +0100

    feat: New example and increased fps

--- renderer/src/window.rs
+++ renderer/src/window.rs
@@ -183,7 +183,7 @@ pub fn create_windows_from_config(
         let resize_timer = env.resizing_timer.clone();
         thread::spawn(move || {
             let time = 1000;
-            let fps_target = 60;
+            let fps_target = 75;
 
             let step = time / fps_target;
             loop {
