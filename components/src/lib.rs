d0e0a98d331e11ac58377a574d88de728bc4a353
marc2332 <mespinsanz@gmail.com>
2022-11-08 22:08:35 +0100

    feat: ThemeProvider component

--- components/src/lib.rs
+++ components/src/lib.rs
@@ -4,6 +4,7 @@ mod router_link;
 mod scroll_views;
 mod slider;
 mod switch;
+mod theme;
 
 pub use button::*;
 pub use input::*;
@@ -11,3 +12,4 @@ pub use router_link::*;
 pub use scroll_views::*;
 pub use slider::*;
 pub use switch::*;
+pub use theme::*;
