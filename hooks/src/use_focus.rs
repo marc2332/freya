bc4d4e617dac42cd77c101e02a76f176e7e282f3
marc2332 <mespinsanz@gmail.com>
2023-01-04 21:55:05 +0100

    feat: use_focus support

--- hooks/src/use_focus.rs
+++ hooks/src/use_focus.rs
@@ -1,25 +1,24 @@
 use dioxus_core::ScopeState;
-use dioxus_hooks::{use_context, use_context_provider};
+use dioxus_hooks::{use_shared_state, use_shared_state_provider};
 use uuid::Uuid;
 
-/// Connect to the Focus provider.
+/// Listen for focus changes, or mark your self as focused. 
 pub fn use_focus(cx: &ScopeState) -> (bool, impl Fn() + '_) {
     let my_id = cx.use_hook(Uuid::new_v4);
-    let focused_id = use_context::<Uuid>(cx);
+    let focused_id = use_shared_state::<Uuid>(cx);
 
-    let focused = Some(*my_id) == focused_id.copied();
+    let is_focused = Some(*my_id) ==  focused_id.map(|v| *v.read());
 
     let focus = move || {
         if let Some(focused_id) = focused_id {
-            //focused_id.borrow_mut() = *my_id;
-            //*focused_id = *my_id;
+            *focused_id.write() = *my_id;
         }
     };
 
-    (focused, focus)
+    (is_focused, focus)
 }
 
-/// Create a Foxus provider.
+/// Create a focus provider.
 pub fn use_init_focus(cx: &ScopeState) {
-    use_context_provider(cx, Uuid::new_v4);
+    use_shared_state_provider(cx, Uuid::new_v4);
 }
