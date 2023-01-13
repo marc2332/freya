e2e2315ed33d85f8b9c5ebcde278d840d2b8ce94
marc2332 <mespinsanz@gmail.com>
2023-01-13 18:12:27 +0100

    feat: use_editable single line multiple editors tests

--- hooks/src/use_editable.rs
+++ hooks/src/use_editable.rs
@@ -393,4 +393,96 @@ mod test {
         assert_eq!(cursor.text(), Some("6:0"));
         assert_eq!(content.text(), Some("Hello! Rustaceans"));
     }
+
+    #[tokio::test]
+    pub async fn single_line_mulitple_editors() {
+        fn use_editable_app(cx: Scope) -> Element {
+            let (content, cursor, process_keyevent, process_clickevent, cursor_reference) =
+                use_editable(
+                    &cx,
+                    || "Hello Rustaceans\nHello World",
+                    EditableMode::SingleLineMultipleEditors,
+                );
+            render!(
+                rect {
+                    width: "100%",
+                    height: "100%",
+                    background: "white",
+                    cursor_reference: cursor_reference,
+
+                    onkeydown: move |e| {
+                        process_keyevent.send(e.data).unwrap();
+                    },
+                    content.lines(0..).enumerate().map(move |(i, line)| {
+                        let process_clickevent = process_clickevent.clone();
+                        rsx!(
+                            paragraph {
+                                width: "100%",
+                                height: "30",
+                                max_lines: "1",
+                                cursor_id: "0",
+                                cursor_index: "{i}",
+                                cursor_color: "black",
+                                cursor_mode: "editable",
+                                onclick:  move |e: MouseEvent| {
+                                    process_clickevent.send((e.data, i)).ok();
+                                },
+                                text {
+                                    color: "black",
+                                    "{line}"
+                                }
+                            }
+                        )
+                    })
+                    label {
+                        color: "black",
+                        height: "50%",
+                        "{cursor.0}:{cursor.1}"
+                    }
+                }
+            )
+        }
+
+        let mut utils = launch_test(use_editable_app);
+
+        // Initial state
+        let root = utils.root().child(0).unwrap();
+        let cursor = root.child(2).unwrap().child(0).unwrap();
+        let content = root.child(0).unwrap().child(0).unwrap().child(0).unwrap();
+        assert_eq!(cursor.text(), Some("0:0"));
+        assert_eq!(content.text(), Some("Hello Rustaceans"));
+
+        // Move cursor
+        utils.send_event(FreyaEvent::Mouse {
+            name: "click",
+            cursor: (35.0, 3.0),
+            button: Some(MouseButton::Left),
+        });
+
+        utils.wait_for_update((500.0, 500.0)).await;
+        utils.wait_for_update((500.0, 500.0)).await;
+
+        // Cursor has been moved
+        let root = utils.root().child(0).unwrap();
+        let cursor = root.child(2).unwrap().child(0).unwrap();
+        assert_eq!(cursor.text(), Some("5:0"));
+
+        // Insert text
+        utils.send_event(FreyaEvent::Keyboard {
+            name: "keydown",
+            code: KeyCode::Character("!"),
+        });
+
+        utils.wait_for_update((500.0, 500.0)).await;
+
+        // Text and cursor have changed
+        let cursor = root.child(2).unwrap().child(0).unwrap();
+        let content = root.child(0).unwrap().child(0).unwrap().child(0).unwrap();
+        assert_eq!(cursor.text(), Some("6:0"));
+        assert_eq!(content.text(), Some("Hello! Rustaceans"));
+
+        // Second line
+        let content = root.child(1).unwrap().child(0).unwrap().child(0).unwrap();
+        assert_eq!(content.text(), Some("Hello World"));
+    }
 }
