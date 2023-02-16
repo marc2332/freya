28ed9dd5daeb889e99bdc59c58aa66a969c17f45
marc2332 <mespinsanz@gmail.com>
2023-02-16 20:34:29 +0100

    fix: Minor change

--- examples/counter.rs
+++ examples/counter.rs
@@ -17,10 +17,10 @@ fn app(cx: Scope) -> Element {
             height: "20%",
             width: "100%",
             background: "rgb(233, 196, 106)",
-            padding: "12.5",
+            padding: "12",
             color: "rgb(20, 33, 61)",
-            label {
-                font_size: "20",
+            label { 
+                font_size: "20", 
                 "Number is: {count}"
             }
         }
@@ -29,7 +29,7 @@ fn app(cx: Scope) -> Element {
             width: "100%",
             background: "rgb(168, 218, 220)",
             color: "black",
-            padding: "12.5",
+            padding: "12",
             onclick: move |_| count += 1,
             label { "Click to increase!" }
         }
