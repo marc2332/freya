5d0fef375aa0f990d930b74eae796d611edf51e7
marc2332 <mespinsanz@gmail.com>
2023-02-18 18:22:19 +0100

    feat: use_animation_manage test

--- hooks/src/use_animation.rs
+++ hooks/src/use_animation.rs
@@ -237,7 +237,7 @@ pub fn use_animation(
 mod test {
     use std::time::Duration;
 
-    use crate::{use_animation, AnimationMode};
+    use crate::{use_animation, use_animation_managed, AnimationMode};
     use dioxus_hooks::use_effect;
     use freya::prelude::*;
     use freya_testing::{launch_test, FreyaEvent, MouseButton};
@@ -332,5 +332,50 @@ mod test {
         assert_eq!(anim.duration(), 500);
         assert_eq!(anim.initial_value(), 7.0);
         assert_eq!(anim.final_value(), 99.0);
+    }
+
+    #[tokio::test]
+    pub async fn test_use_animation_managed() {
+        fn use_animation_app(cx: Scope) -> Element {
+            let (anim, _, progress, _) = use_animation_managed(cx, 0.0);
+
+            use_effect(cx, &(progress), move |v| {
+                if v == 0.0 {
+                    anim(AnimationMode::new_sine_in_out(0.0..=100.0, 50));
+                } else if v == 100.0 {
+                    anim(AnimationMode::new_sine_in_out(100.0..=0.0, 50));
+                }
+                async move {}
+            });
+
+            render!(rect {
+                width: "{progress}",
+            })
+        }
+
+        let mut utils = launch_test(use_animation_app);
+        let element = utils.root().child(0).unwrap();
+
+        // Initial state
+        utils.wait_for_update((500.0, 500.0)).await;
+
+        assert_eq!(element.layout().unwrap().width, 0.0);
+
+        // State somewhere in the middle
+        utils.wait_for_update((500.0, 500.0)).await;
+        utils.wait_for_work((500.0, 500.0)).await;
+        let width = element.layout().unwrap().width;
+        assert!(width > 0.0);
+        assert!(width < 100.0);
+
+        // Reached 100.0
+        utils.wait_until_cleanup((500.0, 500.0)).await;
+        let width = element.layout().unwrap().width;
+        assert_eq!(width, 100.0);
+
+        // Back to 0.0
+        utils.wait_until_cleanup((500.0, 500.0)).await;
+        let width = element.layout().unwrap().width;
+        assert_eq!(width, 0.0);
     }
 }
