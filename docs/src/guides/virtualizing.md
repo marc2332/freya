a8d758a31418fe2645573ae2dd9adaeff83ca13e
marc2332 <mespinsanz@gmail.com>
2023-03-18 00:08:56 +0100

    feat: Virtualizing docs

---
+++ docs/src/guides/virtualizing.md
@@ -0,0 +1,49 @@
+# Virtualizing
+
+Virtualizing helps you render a lot of data efficiently. It will only mount the elements you see in the screen, no matter how big the data is.
+
+### Target usages
+- Text editor
+- Tables
+- Etc
+
+### Usage
+
+Freya comes with a `VirtualScrollView` component which can help you archive the virtualization of some data.
+
+> The virtualization logic of `VirtualScrollView` is implemented at component-level, so, you could implement your own version if you wanted.
+
+Here is an example:
+
+```rust
+fn main() {
+    launch(app);
+}
+
+fn app(cx: Scope) -> Element {
+    let values = use_state(cx, || vec!["Hello World"].repeat(400));
+
+    render!(
+        VirtualScrollView {
+            width: "100%",
+            height: "100%",
+            show_scrollbar: true,
+            length: values.get().len(),
+            item_size: 25.0,
+            builder_values: values.get(),
+            direction: "vertical",
+            builder: Box::new(move |(key, index, values)| {
+                let values = values.unwrap();
+                let value = values[index];
+                rsx! {
+                    label {
+                        key: "{key}",
+                        height: "25",
+                        "{index} {value}"
+                    }
+                }
+            })
+        }
+    )
+}
+```
--- examples/virtual_scroll_view.rs
+++ examples/virtual_scroll_view.rs
@@ -10,125 +10,26 @@ fn main() {
 }
 
 fn app(cx: Scope) -> Element {
-    let (text_editor, process_keyevent, process_clickevent, cursor_ref) = use_editable(
-        cx,
-        || {
-            "Lorem ipsum dolor sit amet\nLorem ipsum dolor sit amet\nLorem ipsum dolor sit amet\nLorem ipsum dolor sit amet\nLorem ipsum dolor sit amet\nLorem ipsum dolor sit amet\nLorem ipsum dolor sit amet"
-        },
-        EditableMode::SingleLineMultipleEditors,
-    );
+    let values = use_state(cx, || vec!["Hello World"].repeat(400));
 
-    let font_size = 20.0;
-    let line_height = 1.2;
-
-    let real_line_height = font_size * line_height;
-
-    render!(
-        container {
-            background: "rgb(15, 15, 15)",
-            padding: "25",
-            direction: "both",
-            width: "auto",
-            height: "50%",
-            onkeydown: move |e| {
-                process_keyevent.send(e.data).unwrap();
-            },
-            cursor_reference: cursor_ref,
-            VirtualScrollView {
-                width: "100%",
-                height: "100%",
-                show_scrollbar: true,
-                length: text_editor.len_lines() as i32,
-                item_size: real_line_height,
-                builder_values: (process_clickevent, text_editor),
-                builder: Box::new(move |(k, line_index, vals)| {
-                    let (process_clickevent, text_editor) = vals.as_ref().unwrap();
-                    let line_content = text_editor.line(line_index as usize).unwrap();
-
-                    let is_line_selected = text_editor.cursor_row() == line_index as usize;
-
-                    // Only show the cursor in the active line
-                    let character_index = if is_line_selected {
-                        text_editor.cursor_col().to_string()
-                    } else {
-                        "none".to_string()
-                    };
-
-                     // Only highlight the active line
-                     let line_background = if is_line_selected {
-                        "rgb(37, 37, 37)"
-                    } else {
-                        ""
-                    };
-
-                    let onmousedown = move |e: MouseEvent| {
-                        process_clickevent.send((e.data, line_index as usize)).ok();
-                    };
-
-                    rsx! {
-                        rect {
-                            key: "{k}",
-                            width: "100%",
-                            height: "{real_line_height}",
-                            direction: "horizontal",
-                            background: "{line_background}",
-                            radius: "7",
-                            rect {
-                                width: "{font_size * 2.0}",
-                                height: "100%",
-                                display: "center",
-                                direction: "horizontal",
-                                label {
-                                    font_size: "{font_size}",
-                                    color: "rgb(200, 200, 200)",
-                                    "{line_index} "
-                                }
-                            }
-                            paragraph {
-                                width: "100%",
-                                cursor_index: "{character_index}",
-                                cursor_color: "white",
-                                max_lines: "1",
-                                cursor_mode: "editable",
-                                cursor_id: "{line_index}",
-                                onmousedown: onmousedown,
-                                text {
-                                    color: "rgb(240, 240, 240)",
-                                    font_size: "{font_size}",
-                                    font_style: "normal",
-                                    "{line_content} "
-                                }
-                            }
-                        }
-                    }
-                })
+    render!(VirtualScrollView {
+        width: "100%",
+        height: "100%",
+        show_scrollbar: true,
+        length: values.get().len(),
+        item_size: 25.0,
+        builder_values: values.get(),
+        direction: "vertical",
+        builder: Box::new(move |(key, index, values)| {
+            let values = values.unwrap();
+            let value = values[index];
+            rsx! {
+                label {
+                    key: "{key}",
+                    height: "25",
+                    "{index} {value}"
+                }
             }
-        }
-        container {
-            background: "rgb(15, 15, 15)",
-            padding: "25",
-            direction: "both",
-            width: "auto",
-            height: "50%",
-            color: "white",
-            VirtualScrollView {
-                width: "100%",
-                height: "100%",
-                show_scrollbar: true,
-                length: 7,
-                item_size: 80.0,
-                builder_values: (),
-                direction: "horizontal",
-                builder: Box::new(move |(k, i, _)| {
-                    rsx! {
-                        label {
-                            key: "{k}",
-                            width: "80",
-                            "Number {i}"
-                        }
-                    }
-                })
-            }
-        }
-    )
+        })
+    })
 }
