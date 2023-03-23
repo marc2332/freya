e97e9f50cf79d0d8f1592c39fcef5f4ff8509126
Marc Espín <mespinsanz@gmail.com>
2023-03-23 18:44:06 +0100

    feat: NetworkImage component (#109)

--- examples/app_dog.rs
+++ examples/app_dog.rs
@@ -3,12 +3,9 @@
     windows_subsystem = "windows"
 )]
 
-use std::time::Duration;
-
 use freya::prelude::*;
 use reqwest::Url;
 use serde::Deserialize;
-use tokio::time::sleep;
 
 fn main() {
     launch(app);
@@ -27,35 +24,14 @@ async fn fetch_random_dog() -> Option<Url> {
     data.message.parse().ok()
 }
 
-async fn fetch_image(url: Url) -> Option<Vec<u8>> {
-    let res = reqwest::get(url).await.ok()?;
-    let data = res.bytes().await.ok()?;
-    Some(data.to_vec())
-}
-
-#[derive(PartialEq)]
-pub enum ImageStatus {
-    Loading,
-    Stopped,
-    Loaded,
-}
-
 fn app(cx: Scope) -> Element {
-    let status = use_state(cx, || ImageStatus::Stopped);
-    let image_bytes = use_state::<Option<Vec<u8>>>(cx, || None);
+    let dog_url = use_state(cx, || None);
 
     let fetch = move || {
-        to_owned![image_bytes, status];
+        to_owned![dog_url];
         cx.spawn(async move {
             if let Some(url) = fetch_random_dog().await {
-                status.set(ImageStatus::Loading);
-                let img = fetch_image(url).await;
-                sleep(Duration::from_millis(1000)).await;
-                if let Some(img) = img {
-                    // Image loaded
-                    image_bytes.set(Some(img));
-                    status.set(ImageStatus::Loaded)
-                }
+                dog_url.set(Some(url))
             }
         })
     };
@@ -65,29 +41,17 @@ fn app(cx: Scope) -> Element {
             background: "rgb(15, 15, 15)",
             width: "100%",
             height: "100%",
+            color: "white",
             container {
                 width: "100%",
                 height: "calc(100% - 58)",
                 radius: "25",
-                display: "center",
-                direction: "both",
-                if *status.get() == ImageStatus::Loading {
-                    rsx!(
-                        Loader {}
-                    )
-                }else if *status.get() == ImageStatus::Loaded {
-                    rsx!{
-                        image_bytes.as_ref().map(|bytes| {
-                            let image_data = bytes_to_data(cx, bytes);
-                            rsx!(
-                                image {
-                                    width: "100%",
-                                    height: "100%",
-                                    image_data: image_data
-                                }
-                            )
-                        })
-                    }
+                if let Some(dog_url) = dog_url.get() {
+                   rsx!(
+                        NetworkImage {
+                            url: dog_url.clone()
+                        }
+                   )
                 }
             }
             container {
