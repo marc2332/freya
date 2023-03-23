c51a234c0853e29695493bd69df2b3adb8babd05
Marc Espín <mespinsanz@gmail.com>
2023-03-23 23:08:59 +0100

    Feat: `use_camera` feature (#122)
    * feat: use_camera feature
    
    * clean up

--- examples/camera.rs
+++ examples/camera.rs
@@ -3,12 +3,19 @@
     windows_subsystem = "windows"
 )]
 
+#[cfg(feature = "use_camera")]
 use freya::prelude::*;
 
+#[cfg(not(feature = "use_camera"))]
+fn main() {
+    panic!("Run with the 'use_camera' feature");
+}
+
+#[cfg(feature = "use_camera")]
 fn main() {
     launch(app);
 }
-
+#[cfg(feature = "use_camera")]
 fn app(cx: Scope) -> Element {
     let (image_reference, camera_error) = use_camera(cx, CameraSettings::default());
 
