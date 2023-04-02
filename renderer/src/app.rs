5f9e0a30d9dfa849cb15e30047163245cd7be7dd
marc2332 <mespinsanz@gmail.com>
2023-04-03 00:07:41 +0200

    fix: Fixed deadlock with devtools enabled

--- renderer/src/app.rs
+++ renderer/src/app.rs
@@ -235,13 +235,13 @@ impl<State: 'static + Clone> App<State> {
                 let dioxus_node = render_node.get_node(dom);
                 if let Some(dioxus_node) = dioxus_node {
                     if let Some(accessibility_id) = dioxus_node.state.accessibility.focus_id {
-                        let children = render_node.get_accessibility_children(&self.rdom.get());
+                        let children = render_node.get_accessibility_children(dom);
                         self.accessibility_state.lock().unwrap().add_element(
                             dioxus_node,
                             render_node,
                             accessibility_id,
                             children,
-                            &self.rdom.get(),
+                            dom,
                         );
                     }
                 }
