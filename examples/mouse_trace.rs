1c0e16a1a8bde99d331fe9536f1287da181a528e
marc2332 <mespinsanz@gmail.com>
2023-04-09 17:11:48 +0200

    example: Simplify mouse_trace example

--- examples/mouse_trace.rs
+++ examples/mouse_trace.rs
@@ -3,10 +3,7 @@
     windows_subsystem = "windows"
 )]
 
-use std::time::Duration;
-
 use freya::prelude::*;
-use tokio::time::interval;
 
 fn main() {
     launch_cfg(
@@ -21,67 +18,31 @@ fn main() {
     );
 }
 
-const MOVEMENT_MARGIN: f64 = 150.0;
-const BOX_COUNT: usize = 15;
-const SPEEDS: (i16, i16, i16) = (2, 3, 6);
+const MOVEMENT_MARGIN: f64 = 100.0;
+const BOX_COUNT: usize = 20;
 
 #[allow(non_snake_case)]
 fn Box(cx: Scope) -> Element {
-    let degrees = use_state(cx, || (0, 0, 0));
-
-    use_effect(cx, (), move |_| {
-        to_owned![degrees];
-        async move {
-            let mut ticker = interval(Duration::from_millis(25));
-            loop {
-                ticker.tick().await;
-
-                degrees.with_mut(|(a, b, c)| {
-                    if *a == 360 {
-                        *a = 0;
-                    }
-                    if *b == 0 {
-                        *b = 360;
-                    }
-                    if *c == 360 {
-                        *c = 0;
-                    }
-                });
-
-                degrees.modify(|(a, b, c)| {
-                    (
-                        (a + SPEEDS.0).clamp(0, 360),
-                        (b - SPEEDS.1).clamp(0, 360),
-                        (c + SPEEDS.2).clamp(0, 360),
-                    )
-                });
-            }
-        }
-    });
-
     render!(
         rect {
-            rotate: "{degrees.0}",
             background: "rgb(65, 53, 67)",
             width: "250",
             height: "250",
             direction: "both",
             display: "center",
-            radius: "10",
+            radius: "100",
             rect {
-                rotate: "{degrees.1}",
                 background: "rgb(143, 67, 238)",
                 width: "180",
                 height: "180",
                 direction: "both",
                 display: "center",
-                radius: "10",
+                radius: "100",
                 rect {
-                    rotate: "{degrees.2}",
                     background: "rgb(240, 235, 141)",
                     width: "100",
                     height: "100",
-                    radius: "10",
+                    radius: "100",
                 }
             }
         }
