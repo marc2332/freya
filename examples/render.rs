133db12c7df8884a750150b01f5bbf0f60807278
marc2332 <mespinsanz@gmail.com>
2023-04-16 13:00:11 +0200

    improvements

--- examples/render.rs
+++ examples/render.rs
@@ -12,6 +12,7 @@ use freya::{
 };
 use freya_node_state::CanvasReference;
 use skia_safe::{Canvas, Color, Font, FontStyle, Paint, Typeface};
+use uuid::Uuid;
 use winit::event_loop::EventLoopProxy;
 
 fn main() {
@@ -19,11 +20,18 @@ fn main() {
 }
 
 struct UseCanvas {
+    id: Uuid,
     renderer: Arc<Box<dyn Fn(&mut Canvas, Area) -> ()>>,
 }
 
+impl PartialEq for UseCanvas {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
 impl UseCanvas {
-    pub fn attribute<'a>(&self, cx: Scope<'a>) -> AttributeValue<'a> {
+    pub fn attribute<'a, T>(&self, cx: Scope<'a, T>) -> AttributeValue<'a> {
         cx.any_value(CustomAttributeValues::Canvas(CanvasReference {
             runner: self.renderer.clone(),
         }))
@@ -34,16 +42,42 @@ fn use_canvas(
     cx: &ScopeState,
     renderer: impl FnOnce() -> Box<dyn Fn(&mut Canvas, Area) -> ()>,
 ) -> UseCanvas {
+    let id = cx.use_hook(Uuid::new_v4);
     let renderer = cx.use_hook(|| Arc::new(renderer()));
 
     UseCanvas {
+        id: id.clone(),
         renderer: renderer.clone(),
     }
 }
 
+#[derive(Props, PartialEq)]
+struct CanvasProps {
+    #[props(default = "300".to_string(), into)]
+    width: String,
+
+    #[props(default = "150".to_string(), into)]
+    height: String,
+
+    #[props(default = "white".to_string(), into)]
+    background: String,
+
+    canvas: UseCanvas,
+}
+
+#[allow(non_snake_case)]
+fn Canvas(cx: Scope<CanvasProps>) -> Element {
+    render!(rect {
+        canvas: cx.props.canvas.attribute(cx),
+        background: "{cx.props.background}",
+        width: "{cx.props.width}",
+        height: "{cx.props.height}",
+    })
+}
+
 fn app(cx: Scope) -> Element {
     let event_loop_proxy = cx.consume_context::<EventLoopProxy<EventMessage>>();
-    let mut state = use_state(cx, || 0);
+    let state = use_state(cx, || 0);
 
     use_effect(cx, (state,), move |_| async move {
         if let Some(event_loop_proxy) = &event_loop_proxy {
@@ -72,16 +106,15 @@ fn app(cx: Scope) -> Element {
                 &font,
                 &text_paint,
             );
+
+            canvas.restore();
         })
     });
 
-    render!(rect {
+    render!(Canvas {
+        canvas: canvas,
         background: "black",
-        onclick: move |_| {
-            state += 1;
-        },
-        canvas: canvas.attribute(cx),
         width: "100%",
-        height: "100%",
+        height: "100%"
     })
 }
