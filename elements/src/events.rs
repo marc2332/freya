a43af8b396bcb9d95dfa136b8b97b5b0d5270b99
Marc Espín <mespinsanz@gmail.com>
2023-06-03 19:10:57 +0200

    feat: Pointer events (#203)
    * feat: Pointer events
    
    * update tests
    
    * invalidate ci cache
    
    * improvements
    
    * clean up
    
    * tests
    
    * rename
    
    * remove debris
    
    * clean up code

--- elements/src/events.rs
+++ elements/src/events.rs
@@ -1,11 +1,13 @@
 pub mod keyboard;
 pub mod mouse;
+pub mod pointer;
 pub mod touch;
 pub mod wheel;
 
 use dioxus_core::Event;
 pub use keyboard::KeyboardData;
 pub use mouse::MouseData;
+pub use pointer::PointerData;
 pub use touch::TouchData;
 pub use wheel::WheelData;
 
@@ -13,3 +15,4 @@ pub type KeyboardEvent = Event<KeyboardData>;
 pub type MouseEvent = Event<MouseData>;
 pub type WheelEvent = Event<WheelData>;
 pub type TouchEvent = Event<TouchData>;
+pub type PointerEvent = Event<PointerData>;
