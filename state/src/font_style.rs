45e9d8381c45ad429b9cffc9d3b21a16daeedced
Tropical <42101043+Tropix126@users.noreply.github.com>
2023-06-08 23:05:45 +0100

    allow weights of 50 and 950

--- state/src/font_style.rs
+++ state/src/font_style.rs
@@ -218,7 +218,7 @@ fn parse_font_weight(weight: &str) -> Weight {
         "extra-bold" => Weight::EXTRA_BOLD,
         "black" => Weight::BLACK,
         "extra-black" => Weight::EXTRA_BLACK,
-        // "50" => skia_safe::font_style::Weight::INVISIBLE,
+        "50" => skia_safe::font_style::Weight::INVISIBLE,
         "100" => Weight::THIN,
         "200" => Weight::EXTRA_LIGHT,
         "300" => Weight::LIGHT,
@@ -228,7 +228,7 @@ fn parse_font_weight(weight: &str) -> Weight {
         "700" => Weight::BOLD,
         "800" => Weight::EXTRA_BOLD,
         "900" => Weight::BLACK,
-        // "950" => Weight::EXTRA_BLACK,
+        "950" => Weight::EXTRA_BLACK,
         _ => Weight::NORMAL,
     }
 }
