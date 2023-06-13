9557aa01d09c50eec50939f6b7f4e1e06b7a3637
Tropical <42101043+Tropix126@users.noreply.github.com>
2023-06-13 19:39:10 +0100

    comment shadowproperty

--- devtools/src/property.rs
+++ devtools/src/property.rs
@@ -82,64 +82,64 @@ pub fn ColorfulProperty<'a>(cx: Scope<'a>, name: &'a str, color: &'a Color) -> E
     )
 }
 
-#[allow(non_snake_case)]
-#[inline_props]
-pub fn ShadowProperty<'a>(
-    cx: Scope<'a>,
-    name: &'a str,
-    // shadow_settings: &'a ShadowSettings,
-) -> Element<'a> {
-    // let color = shadow_settings.color.to_rgb();
-    render!(
-        container {
-            height: "30",
-            width: "100%",
-            direction: "horizontal",
-            padding: "10",
-            paragraph {
-                text {
-                    font_size: "15",
-                    color: "rgb(71, 180, 240)",
-                    "{name}"
-                }
-                text {
-                    font_size: "15",
-                    color: "rgb(215, 215, 215)",
-                    ": "
-                }
-                // text {
-                //     font_size: "15",
-                //     color: "rgb(252,181,172)",
-                //     "{shadow_settings.x} {shadow_settings.y} {shadow_settings.intensity} {shadow_settings.size}"
-                // }
-            }
-            rect {
-                width: "5"
-            }
-            rect {
-                width: "17",
-                height: "17",
-                radius: "5",
-                background: "white",
-                padding: "2.5",
-                rect {
-                    radius: "3",
-                    width: "100%",
-                    height: "100%",
-                    background: "rgb({color.r}, {color.g}, {color.b})",
-                }
-            }
-            rect {
-                width: "5"
-            }
-            label {
-                font_size: "15",
-                color: "rgb(252,181,172)",
-                "rgb({color.r}, {color.g}, {color.b})"
-            }
-        }
-    )
-}
+// #[allow(non_snake_case)]
+// #[inline_props]
+// pub fn ShadowProperty<'a>(
+//     cx: Scope<'a>,
+//     name: &'a str,
+//     shadow_settings: &'a ShadowSettings,
+// ) -> Element<'a> {
+//     let color = shadow_settings.color.to_rgb();
+//     render!(
+//         container {
+//             height: "30",
+//             width: "100%",
+//             direction: "horizontal",
+//             padding: "10",
+//             paragraph {
+//                 text {
+//                     font_size: "15",
+//                     color: "rgb(71, 180, 240)",
+//                     "{name}"
+//                 }
+//                 text {
+//                     font_size: "15",
+//                     color: "rgb(215, 215, 215)",
+//                     ": "
+//                 }
+//                 text {
+//                     font_size: "15",
+//                     color: "rgb(252,181,172)",
+//                     "{shadow_settings.x} {shadow_settings.y} {shadow_settings.intensity} {shadow_settings.size}"
+//                 }
+//             }
+//             rect {
+//                 width: "5"
+//             }
+//             rect {
+//                 width: "17",
+//                 height: "17",
+//                 radius: "5",
+//                 background: "white",
+//                 padding: "2.5",
+//                 rect {
+//                     radius: "3",
+//                     width: "100%",
+//                     height: "100%",
+//                     background: "rgb({color.r}, {color.g}, {color.b})",
+//                 }
+//             }
+//             rect {
+//                 width: "5"
+//             }
+//             label {
+//                 font_size: "15",
+//                 color: "rgb(252,181,172)",
+//                 "rgb({color.r}, {color.g}, {color.b})"
+//             }
+//         }
+//     )
+// }
 
 #[allow(non_snake_case)]
 #[inline_props]
