01611c2678df368df5d7e0e5051072d7ede78e20
Tropical <42101043+Tropix126@users.noreply.github.com>
2023-06-13 19:00:24 +0100

    update tests

--- state/tests/parse_shadows.rs
+++ state/tests/parse_shadows.rs
@@ -9,9 +9,42 @@ fn parse_big_shadow() {
         Some(ShadowSettings {
             x: 1.0,
             y: 2.0,
-            intensity: 50,
-            size: 25.0,
-            color: Color::RED
+            blur: 50.0,
+            spread: 25.0,
+            color: Color::RED,
+            inset: false
+        })
+    );
+}
+
+#[test]
+fn parse_inset_shadow() {
+    let shadow = parse_shadow("inset 1 2 50 25.0 red");
+    assert_eq!(
+        shadow,
+        Some(ShadowSettings {
+            x: 1.0,
+            y: 2.0,
+            blur: 50.0,
+            spread: 25.0,
+            color: Color::RED,
+            inset: true
+        })
+    );
+}
+
+#[test]
+fn parse_shadow_with_assumed_spread() {
+    let shadow = parse_shadow("inset 1 2 50 red");
+    assert_eq!(
+        shadow,
+        Some(ShadowSettings {
+            x: 1.0,
+            y: 2.0,
+            blur: 50.0,
+            spread: 0.0,
+            color: Color::RED,
+            inset: true
         })
     );
 }
