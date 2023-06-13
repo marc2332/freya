7bc994edc7ff73d8e585f6e4c504a8e375a4bd69
marc2332 <mespinsanz@gmail.com>
2023-06-13 23:01:40 +0200

    updated tests

--- state/tests/parse_gap.rs
+++ state/tests/parse_gap.rs
@@ -4,19 +4,19 @@ use torin::gap::Gap;
 const SCALE_FACTOR: f32 = 1.0;
 
 #[test]
-fn parse_all_paddings() {
-    let paddings = parse_gap("10", SCALE_FACTOR);
-    assert_eq!(paddings, Some(Gap::new(10.0, 10.0, 10.0, 10.0)));
+fn parse_all_gaps() {
+    let gaps = parse_gap("10", SCALE_FACTOR);
+    assert_eq!(gaps, Some(Gap::new(10.0, 10.0, 10.0, 10.0)));
 }
 
 #[test]
-fn parse_axis_paddings() {
-    let paddings = parse_gap("50 10", SCALE_FACTOR);
-    assert_eq!(paddings, Some(Gap::new(50.0, 10.0, 50.0, 10.0)));
+fn parse_axis_gaps() {
+    let gaps = parse_gap("50 10", SCALE_FACTOR);
+    assert_eq!(gaps, Some(Gap::new(50.0, 10.0, 50.0, 10.0)));
 }
 
 #[test]
-fn parse_sides_paddings() {
-    let paddings = parse_gap("1 2 3 4", SCALE_FACTOR);
-    assert_eq!(paddings, Some(Gap::new(1.0, 2.0, 3.0, 4.0)));
+fn parse_sides_gaps() {
+    let gaps = parse_gap("1 2 3 4", SCALE_FACTOR);
+    assert_eq!(gaps, Some(Gap::new(1.0, 2.0, 3.0, 4.0)));
 }
