0119d935d38d55c32cd00e2163b96c8667a136d6
marc2332 <mespinsanz@gmail.com>
2023-06-18 13:21:03 +0200

    fixes tests

--- state/tests/parse_gaps.rs
+++ state/tests/parse_gaps.rs
@@ -1,21 +1,20 @@
+use freya_node_state::Parse;
 use torin::gaps::Gaps;
-
-const SCALE_FACTOR: f32 = 1.0;
 
 #[test]
 fn parse_all_gaps() {
-    let gaps = Gaps::parse("10", SCALE_FACTOR);
-    assert_eq!(gaps, Some(Gaps::new(10.0, 10.0, 10.0, 10.0)));
+    let gaps = Gaps::parse("10");
+    assert_eq!(gaps, Ok(Gaps::new(10.0, 10.0, 10.0, 10.0)));
 }
 
 #[test]
 fn parse_axis_gaps() {
-    let gaps = Gaps::parse("50 10", SCALE_FACTOR);
-    assert_eq!(gaps, Some(Gaps::new(50.0, 10.0, 50.0, 10.0)));
+    let gaps = Gaps::parse("50 10");
+    assert_eq!(gaps, Ok(Gaps::new(50.0, 10.0, 50.0, 10.0)));
 }
 
 #[test]
 fn parse_sides_gaps() {
-    let gaps = Gaps::parse("1 2 3 4", SCALE_FACTOR);
-    assert_eq!(gaps, Some(Gaps::new(1.0, 2.0, 3.0, 4.0)));
+    let gaps = Gaps::parse("1 2 3 4");
+    assert_eq!(gaps, Ok(Gaps::new(1.0, 2.0, 3.0, 4.0)));
 }
