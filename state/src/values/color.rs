88107f2a07be95ff1647c1536d32403956ad61b9
Tropical <42101043+Tropix126@users.noreply.github.com>
2023-06-21 12:50:20 +0200

    HSL color support

--- state/src/values/color.rs
+++ state/src/values/color.rs
@@ -1,5 +1,5 @@
 use crate::Parse;
-use skia_safe::Color;
+use skia_safe::{Color, HSV};
 
 #[derive(Debug, PartialEq, Eq)]
 pub struct ParseColorError;
@@ -18,7 +18,13 @@ impl Parse for Color {
             "white" => Ok(Color::WHITE),
             "orange" => Ok(Color::from_rgb(255, 165, 0)),
             "transparent" => Ok(Color::TRANSPARENT),
-            _ => parse_rgb(value),
+            _ => {
+                if value.starts_with("hsl") {
+                    parse_hsl(value)
+                } else {
+                    parse_rgb(value)
+                }
+            }
         }
     }
 }
@@ -52,5 +58,57 @@ fn parse_rgb(color: &str) -> Result<Color, ParseColorError> {
         Ok(Color::from_argb(a, r, g, b))
     } else {
         Ok(Color::from_rgb(r, g, b))
+    }
+}
+
+fn parse_hsl(color: &str) -> Result<Color, ParseColorError> {
+    let color = color.replace("hsl(", "").replace(')', "");
+    let mut colors = color.split(',');
+
+    // Get each color component
+    let h = colors.next()
+        .ok_or(ParseColorError)?
+        .trim().replace("deg", "")
+        .parse::<f32>()
+        .map_err(|_| ParseColorError)?;
+    let s_str = colors.next().ok_or(ParseColorError)?.trim();
+    let l_str = colors.next().ok_or(ParseColorError)?.trim();
+    let a_str: Option<&str> = colors.next();
+
+    // S, L and A can end in percentage, otherwise its 0.0 - 1.0
+    let mut s = if s_str.ends_with("%") {
+        s_str.replace("%", "").parse::<f32>().map_err(|_| ParseColorError)? / 100.0
+    } else {
+        s_str.parse::<f32>().map_err(|_| ParseColorError)?
+    };
+
+    let mut l = if l_str.ends_with("%") {
+        l_str.replace("%", "").parse::<f32>().map_err(|_| ParseColorError)? / 100.0
+    } else {
+        l_str.parse::<f32>().map_err(|_| ParseColorError)?
+    };
+
+    // HSL to HSV Conversion
+    l *= 2.0;
+    s *= if l <= 1.0 {
+        l
+    } else {
+        2.0 - l
+    };
+    let v = (l + s) / 2.0;
+    s = (2.0 * s) / (l + s);
+    let hsv = HSV { h, s, v };
+
+    // Handle alpha formatting and convert to ARGB
+    if let Some(a_str) = a_str {
+        let a = if a_str.ends_with("%") {
+            a_str.trim().replace("%", "").parse::<f32>().map_err(|_| ParseColorError)? / 100.0
+        } else {
+            a_str.trim().parse::<f32>().map_err(|_| ParseColorError)?
+        };
+
+        Ok(hsv.to_color((a * 255.0).round() as u8))
+    } else {
+        Ok(hsv.to_color(255))
     }
 }
--- state/tests/parse_color.rs
+++ state/tests/parse_color.rs
@@ -9,6 +9,14 @@ fn parse_manual_color() {
 
 #[test]
 fn parse_rgb_color() {
-    let color = Color::parse("rgb(91, 123, 57");
+    let color = Color::parse("rgb(91, 123, 57)");
     assert_eq!(color, Ok(Color::from_rgb(91, 123, 57)));
+}
+
+#[test]
+fn parse_hsl_color() {
+    let color = Color::parse("hsl(28, 0.8, 0.5, 0.25)").unwrap();
+    let color_pct = Color::parse("hsl(28deg, 80%, 50%, 25%)").unwrap();
+
+    assert_eq!(color, color_pct);
 }
