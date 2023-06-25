b09241c193c07a6fa46eebe3b3c230df810673e7
Tropical <42101043+Tropix126@users.noreply.github.com>
2023-06-25 02:49:02 +0100

    make corner_smoothing use percentage

--- state/src/style.rs
+++ state/src/style.rs
@@ -116,9 +116,12 @@ impl State<CustomAttributeValues> for Style {
                     }
                     "corner_smoothing" => {
                         if let Some(value) = attr.value.as_text() {
-                            if let Ok(smoothing) = value.parse::<f32>() {
-                                style.corner_radius.smoothing = smoothing;
+                            if !value.ends_with("%") {
+                                if let Ok(smoothing) = value.replacen("%", "", 1).parse::<f32>() {
+                                    style.corner_radius.smoothing = smoothing / 100.0;
+                                }
                             }
+
                         }
                     }
                     "image_data" => {
