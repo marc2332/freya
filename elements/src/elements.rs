1d3975d31c500f124ca0bad143053b8c794d8b6f
marc2332 <mespinsanz@gmail.com>
2023-08-15 12:47:04 +0200

    feat: Enable `font_family` in rect

--- elements/src/elements.rs
+++ elements/src/elements.rs
@@ -172,6 +172,7 @@ builder_constructors! {
         corner_radius: String,
         corner_smoothing: String,
         color: String,
+        font_family: String,
         display: String,
         reference: Reference,
         cursor_reference: CursorReference,
