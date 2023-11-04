536e9e337ab661aed157ef0cfbc621b16b282f64
marc2332 <mespinsanz@gmail.com>
2023-11-04 11:14:05 +0100

    improvements

--- crates/components/src/scroll_views/scroll_bar.rs
+++ crates/components/src/scroll_views/scroll_bar.rs
@@ -1,6 +1,6 @@
 use dioxus::prelude::*;
 use freya_elements::elements as dioxus_elements;
-use freya_hooks::{use_get_theme, ScrollbarTheme};
+use freya_hooks::use_get_theme;
 
 #[derive(Props)]
 pub struct ScrollBarProps<'a> {
@@ -17,12 +17,29 @@ pub struct ScrollBarProps<'a> {
 
     #[props(default = "0".to_string(), into)]
     offset_y: String,
+
+    clicking_scrollbar: bool,
+}
+
+enum ScrollBarStatus {
+    Idle,
+    Hovering,
 }
 
 #[allow(non_snake_case)]
 pub fn ScrollBar<'a>(cx: Scope<'a, ScrollBarProps<'a>>) -> Element<'a> {
+    let status = use_state(cx, || ScrollBarStatus::Idle);
     let theme = use_get_theme(cx);
-    let ScrollbarTheme { background, .. } = &theme.scrollbar;
+
+    let onmouseenter = |_| status.set(ScrollBarStatus::Hovering);
+
+    let onmouseleave = |_| status.set(ScrollBarStatus::Idle);
+
+    let background = match status.get() {
+        _ if cx.props.clicking_scrollbar => theme.scrollbar.background,
+        ScrollBarStatus::Hovering => theme.scrollbar.background,
+        ScrollBarStatus::Idle => "transparent",
+    };
 
     render!(
         rect {
@@ -33,6 +50,8 @@ pub fn ScrollBar<'a>(cx: Scope<'a, ScrollBarProps<'a>>) -> Element<'a> {
             offset_x: "{cx.props.offset_x}",
             offset_y: "{cx.props.offset_y}",
             background: "{background}",
+            onmouseenter: onmouseenter,
+            onmouseleave: onmouseleave,
             &cx.props.children
         }
     )
