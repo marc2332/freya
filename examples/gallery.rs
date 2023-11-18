88acd4febf52d49f60ce53b8d43e6256b16dc83a
marc2332 <mespinsanz@gmail.com>
2023-11-18 22:35:13 +0100

    improvements

--- examples/gallery.rs
+++ examples/gallery.rs
@@ -24,6 +24,8 @@ pub enum Route {
         ButtonDemo,
         #[route("/scrollview")]
         ScrollViewDemo,
+        #[route("/dropdown")]
+        DropdownDemo,
     #[end_layout]
     #[route("/..route")]
     PageNotFound { },
@@ -78,12 +80,11 @@ fn ButtonDemo(cx: Scope) -> Element {
     let mut count = use_state(cx, || 4);
 
     render!(
-        label {
-            font_size: "40",
+        DemoTitle {
             "Button"
         }
-        label {
-            "Example:"
+        SectionTitle {
+            "Example"
         }
         DemoBlock {
             Button {
@@ -98,16 +99,129 @@ fn ButtonDemo(cx: Scope) -> Element {
 
 #[allow(non_snake_case)]
 fn ScrollViewDemo(cx: Scope) -> Element {
+    let show_scrollbar = use_state(cx, || true);
+
     render!(
-        label {
-            font_size: "40",
+        DemoTitle {
             "ScrollView"
         }
-        label {
-            "Example:"
+        SectionTitle {
+            "Example"
         }
-
+        DemoBlock {
+            ScrollView {
+                height: "200",
+                width: "250",
+                show_scrollbar: *show_scrollbar.get(),
+                
+                rect {
+                    direction: "horizontal",
+                    rect {
+                        height: "80",
+                        width: "120",
+                        background: "rgb(236, 143, 94)"
+                    }
+                    rect {
+                        height: "80",
+                        width: "280",
+                        background: "rgb(236, 227, 206)"
+                    }
+                }
+                rect {
+                    direction: "horizontal",
+                    rect {
+                        height: "70",
+                        width: "190",
+                        background: "rgb(243, 182, 100)"
+                    }
+                    rect {
+                        height: "70",
+                        width: "210",
+                        background: "rgb(115, 144, 114)"
+                    }
+                }
+                rect {
+                    direction: "horizontal",
+                    rect {
+                        height: "60",
+                        width: "260",
+                        background: "rgb(241, 235, 144)"
+                    }
+                    rect {
+                        height: "60",
+                        width: "140",
+                        background: "rgb(79, 111, 82)"
+                    }
+                }
+                rect {
+                    direction: "horizontal",
+                    rect {
+                        height: "50",
+                        width: "330",
+                        background: "rgb(159, 187, 115)"
+                    }
+                    rect {
+                        height: "50",
+                        width: "70",
+                        background: "rgb(58, 77, 57)"
+                    }
+                }
+            }
+        }
+        SmallTitle {
+            "Show scrollbar?"
+        }
+        Switch {
+            enabled: *show_scrollbar.get(),
+            ontoggled: |_| show_scrollbar.set(!*show_scrollbar.get())
+        }
     )
+}
+
+#[allow(non_snake_case)]
+fn DropdownDemo(cx: Scope) -> Element {
+    render!(
+        DemoTitle {
+            "Dropdown"
+        }
+        SectionTitle {
+            "Example"
+        }
+    )
+}
+
+#[allow(non_snake_case)]
+#[inline_props]
+fn DemoTitle<'a>(cx: Scope<'a>, children: Element<'a>) -> Element {
+    render!(label {
+        font_size: "22",
+        margin: "6 0 18 0",
+        color: "rgb(20, 20, 20)",
+        children
+    })
+}
+
+
+#[allow(non_snake_case)]
+#[inline_props]
+fn SectionTitle<'a>(cx: Scope<'a>, children: Element<'a>) -> Element {
+    render!(label {
+        font_size: "18",
+        margin: "4 0 12 0",
+        color: "rgb(45, 45, 45)",
+        children
+    })
+}
+
+#[allow(non_snake_case)]
+#[inline_props]
+fn SmallTitle<'a>(cx: Scope<'a>, children: Element<'a>) -> Element {
+    render!(label {
+        font_size: "16",
+        margin: "8 0 2 0",
+        color: "rgb(25, 25, 25)",
+        children
+    })
 }
 
 #[allow(non_snake_case)]
@@ -116,6 +230,7 @@ fn SmallText<'a>(cx: Scope<'a>, children: Element<'a>) -> Element {
     render!(label {
         font_size: "14",
         margin: "4 0 2 0",
+        color: "rgb(70, 70, 70)",
         children
     })
 }
