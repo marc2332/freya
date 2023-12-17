714c9bb9503df3f2042c272449c4038784642ed8
marc2332 <mespinsanz@gmail.com>
2023-12-17 17:36:26 +0100

    tests

--- crates/torin/tests/size.rs
+++ crates/torin/tests/size.rs
@@ -1,3 +1,4 @@
+use euclid::Length;
 use torin::{prelude::*, test_utils::*};
 
 #[test]
@@ -584,5 +585,59 @@ pub fn fill_size() {
     assert_eq!(
         layout.get(2).unwrap().visible_area().round(),
         Rect::new(Point2D::new(0.0, 300.0), Size2D::new(1000.0, 700.0)),
+    );
+}
+
+#[test]
+pub fn root_percentage() {
+    let (mut layout, mut measurer) = test_utils();
+
+    let mut mocked_dom = TestingDOM::default();
+    mocked_dom.add(
+        0,
+        None,
+        vec![1],
+        Node::from_size_and_direction(
+            Size::Percentage(Length::new(100.0)),
+            Size::Percentage(Length::new(100.0)),
+            DirectionMode::Vertical,
+        ),
+    );
+    mocked_dom.add(
+        1,
+        Some(0),
+        vec![2],
+        Node::from_size_and_direction(
+            Size::Percentage(Length::new(50.0)),
+            Size::Pixels(Length::new(300.0)),
+            DirectionMode::Vertical,
+        ),
+    );
+    mocked_dom.add(
+        2,
+        Some(1),
+        vec![],
+        Node::from_size_and_direction(
+            Size::RootPercentage(Length::new(75.0)),
+            Size::Pixels(Length::new(100.0)),
+            DirectionMode::Vertical,
+        ),
+    );
+
+    layout.measure(
+        0,
+        Rect::new(Point2D::new(0.0, 0.0), Size2D::new(1000.0, 1000.0)),
+        &mut measurer,
+        &mut mocked_dom,
+    );
+
+    assert_eq!(
+        layout.get(1).unwrap().visible_area().round(),
+        Rect::new(Point2D::new(0.0, 0.0), Size2D::new(500.0, 300.0)),
+    );
+
+    assert_eq!(
+        layout.get(2).unwrap().visible_area().round(),
+        Rect::new(Point2D::new(0.0, 0.0), Size2D::new(750.0, 100.0)),
     );
 }
