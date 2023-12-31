f3a9d07f6307fa9cdc21ee6b0e11fb528e490d22
marc2332 <mespinsanz@gmail.com>
2023-12-31 12:27:22 +0100

    remove react astro plugin

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from 'astro/config';
 import { rehypeAccessibleEmojis } from 'rehype-accessible-emojis';
 import rehypeAutoLinks from 'rehype-autolink-headings';
 import rehypeSlug from 'rehype-slug'
-import react from "@astrojs/react";
 import deno from "@astrojs/deno";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [tailwind()],
   output: "server",
   adapter: deno(),
   markdown: {
