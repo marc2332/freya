20a0780b58043575545cabd9bcd834eee6a2c32e
marc2332 <mespinsanz@gmail.com>
2024-01-01 11:31:23 +0100

    chore: cargo fmt

--- crates/renderer/src/window.rs
+++ crates/renderer/src/window.rs
@@ -48,7 +48,8 @@ pub struct WindowEnv<T: Clone> {
 
 impl<T: Clone> Drop for WindowEnv<T> {
     fn drop(&mut self) {
-        if !self.gl_context.is_current() && self.gl_context.make_current(&self.gl_surface).is_err() {
+        if !self.gl_context.is_current() && self.gl_context.make_current(&self.gl_surface).is_err()
+        {
             self.gr_context.abandon();
         }
     }
