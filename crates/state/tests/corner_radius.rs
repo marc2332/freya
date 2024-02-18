f2cc27455886e06e034d712f3a36a4dcff41cf08
marc2332 <mespinsanz@gmail.com>
2024-02-18 21:50:12 +0100

    fix

--- crates/state/tests/corner_radius.rs
+++ crates/state/tests/corner_radius.rs
@@ -6,14 +6,22 @@ fn smooth_corner_radius() {
     let corner_radius = CornerRadius {
         top_left: 12.,
         top_right: 10.,
-        bottom_left: 5.0,
-        bottom_right: 3.0,
+        bottom_left: 5.,
+        bottom_right: 3.,
         smoothing: 20.,
     };
 
-    let rect = RRect::default();
+    let rounded_rect = RRect::new_rect_radii(
+        Rect::new(5., 12., 8., 60.),
+        &[
+            (15., 15.).into(),
+            (30., 30.).into(),
+            (5., 5.).into(),
+            (20., 20.).into(),
+        ],
+    );
 
-    let path = corner_radius.smoothed_path(rect);
+    let path = corner_radius.smoothed_path(rounded_rect);
 
     assert!(path.is_valid());
     assert!(path.is_last_contour_closed());
