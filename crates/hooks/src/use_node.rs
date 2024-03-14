8e56061512e9d4b2cca2e9c5c0cc90a38d2c2f72
marc2332 <mespinsanz@gmail.com>
2024-03-14 18:25:35 +0100

    tweak

--- crates/hooks/src/use_node.rs
+++ crates/hooks/src/use_node.rs
@@ -25,7 +25,7 @@ pub fn use_node() -> (AttributeValue, NodeReferenceLayout) {
 
     (
         AttributeValue::any_value(CustomAttributeValues::Reference(NodeReference(tx))),
-        node_reference_layout.read().clone(),
+        signal.read_unchecked().clone(),
     )
 }
 
