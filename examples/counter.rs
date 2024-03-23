78668f63606198fc15bf7e182a627b1d321ccfd7
marc2332 <mespinsanz@gmail.com>
2024-03-23 11:11:14 +0100

    tweka

--- examples/counter.rs
+++ examples/counter.rs
@@ -14,22 +14,32 @@ fn app() -> Element {
 
     rsx!(
         rect {
-            height: "100%",
+            height: "50%",
             width: "100%",
-            padding: "30",
-            background: "red",
-            rect {
-                height: "100%",
-                width: "100%",
-                padding: "30",
-                background: "green",
-                rect {
-                    height: "100%",
-                    width: "100%",
-                    padding: "30",
-                    background: "blue",
-
-                }
+            main_align: "center",
+            cross_align: "center",
+            background: "rgb(0, 119, 182)",
+            color: "white",
+            shadow: "0 4 20 5 rgb(0, 0, 0, 80)",
+            label {
+                font_size: "75",
+                font_weight: "bold",
+                "{count}"
+            }
+        }
+        rect {
+            height: "50%",
+            width: "100%",
+            main_align: "center",
+            cross_align: "center",
+            direction: "horizontal",
+            Button {
+                onclick: move |_| count += 1,
+                label { "Increase" }
+            }
+            Button {
+                onclick: move |_| count -= 1,
+                label { "Decrease" }
             }
         }
     )
