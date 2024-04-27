46c2be537b3214d09f00b13804fba8c3b1272483
marc2332 <mespinsanz@gmail.com>
2024-04-27 11:52:54 +0200

    revert animation.rs example

--- examples/animation.rs
+++ examples/animation.rs
@@ -10,24 +10,58 @@ fn main() {
 }
 
 fn app() -> Element {
-    let animation = use_animation(|ctx| {
-        ctx.auto_start(true);
-        ctx.on_finish(OnFinish::Reverse);
+    let mut toggle = use_signal(|| true);
+    let animations = use_animation(|ctx| {
         (
-            ctx.with(AnimNum::new(0., 100.).time(200)),
-            ctx.with(AnimColor::new("red", "blue").time(200))
+            ctx.with(
+                AnimNum::new(100., 200.)
+                    .time(500)
+                    .ease(Ease::Out)
+                    .function(Function::Expo),
+            ),
+            ctx.with(
+                AnimColor::new("rgb(131, 111, 255)", "rgb(255, 167, 50)")
+                    .time(170)
+                    .ease(Ease::InOut),
+            ),
+            ctx.with(
+                AnimNum::new(0., 360.)
+                    .time(1000)
+                    .ease(Ease::Out)
+                    .function(Function::Bounce),
+            ),
+            ctx.with(
+                AnimNum::new(50., 0.)
+                    .time(550)
+                    .ease(Ease::InOut)
+                    .function(Function::Bounce),
+            ),
         )
     });
-    
-    let (width, color) = animation.get();
-    let width = width.read().as_f32();
-    let background = color.read().as_string();
+
+    let (size, color, rotate, radius) = animations.get();
 
     rsx!(
         rect {
+            main_align: "center",
+            cross_align: "center",
             height: "100%",
-            width: "{width}",
-            background: "{background}"
+            width: "100%",
+            onclick: move |_| {
+                if *toggle.peek() {
+                    animations.start();
+                } else {
+                    animations.reverse();
+                }
+                toggle.toggle();
+            },
+            rect {
+                width: "{size.read().as_f32()}",
+                rotate: "{rotate.read().as_f32()}deg",
+                height: "50%",
+                background: "{color.read().as_string()}",
+                corner_radius: "{radius.read().as_f32()}"
+            }
         }
     )
 }
