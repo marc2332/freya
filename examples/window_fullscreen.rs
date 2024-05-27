1359d8ce991d7e638623942fcc489c21284586ba
dload0 <dloadzer0@gmail.com>
2024-05-27 22:24:25 +0100

    fix: Update to new API.
    Co-authored-by: Marc Espin <mespinsanz@gmail.com>

--- examples/window_fullscreen.rs
+++ examples/window_fullscreen.rs
@@ -11,17 +11,17 @@ fn main() {
 
 fn app() -> Element {
 
+    let platform = use_platform();
+
     rsx!(
         rect {
             width: "100%",
             height: "100%",
-
             main_align: "center",
             cross_align: "center",
             direction: "horizontal",
-
             Button {
-                onclick: |_| use_platform().fullscreen_window(),
+                onclick: move |_| platform.toggle_fullscreen_window(),
                 label {
                     "Toggle fullscreen"
                 }
