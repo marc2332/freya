e6cceb6ce541cb07d6d33c16893d14bd9e93da86
marc2332 <mespinsanz@gmail.com>
2024-05-31 19:27:45 +0200

    fix: revert some changes

--- crates/hooks/src/use_platform_information.rs
+++ crates/hooks/src/use_platform_information.rs
@@ -1,8 +1,8 @@
 use dioxus_hooks::use_context;
-use dioxus_signals::Memo;
+use dioxus_signals::Signal;
 use freya_core::prelude::PlatformInformation;
 
 /// Get access to information from the platform.
-pub fn use_platform_information() -> Memo<PlatformInformation> {
+pub fn use_platform_information() -> Signal<PlatformInformation> {
     use_context()
 }
