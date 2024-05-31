128b9623bfc2856f62d7f4932ce895152afa02ec
Marc Espin <mespinsanz@gmail.com>
2024-05-31 21:55:20 +0200

    feat: `WithWindow` event (#626)
    * feat: With window event
    
    * clean up
    
    * feat: `min/max/fullscreen window` in `use_platform`. (#627)
    
    * feat: `min/max/fullscreen window` in `use_platform`.
    
    Added window features using the new `with_window` event.
    
    - `maximize_window`: When called, maximizes the window if not maximized, or de-maximizes the window if already maximized (standard behaviour).
    - `minimize_window`: When called, minimizes the window.
    - `fullscreen_window`: When called, fullscreens the window if not fullscreened, or de-fullscreens the window if already fullscreen.
    
    * fix: Update to new API.
    
    Co-authored-by: Marc Espin <mespinsanz@gmail.com>
    
    * fix: Update to new API.
    
    Co-authored-by: Marc Espin <mespinsanz@gmail.com>
    
    * fix: Update to new API.
    
    Co-authored-by: Marc Espin <mespinsanz@gmail.com>
    
    * fix: Update to new API.
    
    Co-authored-by: Marc Espin <mespinsanz@gmail.com>
    
    * fix: Update to new API.
    
    Co-authored-by: Marc Espin <mespinsanz@gmail.com>
    
    * chore: Clean up
    
    ---------
    
    Co-authored-by: Marc Espin <mespinsanz@gmail.com>
    
    ---------
    
    Co-authored-by: dload0 <dloadzer0@gmail.com>

--- crates/hooks/src/use_platform.rs
+++ crates/hooks/src/use_platform.rs
@@ -5,7 +5,12 @@ use dioxus_signals::{Readable, Signal};
 use freya_common::EventMessage;
 use tokio::sync::{broadcast, mpsc::UnboundedSender};
 use torin::geometry::Size2D;
-use winit::{dpi::PhysicalSize, event_loop::EventLoopProxy, window::CursorIcon};
+use winit::window::Fullscreen;
+use winit::{
+    dpi::PhysicalSize,
+    event_loop::EventLoopProxy,
+    window::{CursorIcon, Window},
+};
 
 #[derive(Clone, Copy, PartialEq)]
 pub struct UsePlatform {
@@ -49,8 +54,62 @@ impl UsePlatform {
         self.send(EventMessage::SetCursorIcon(cursor_icon)).ok();
     }
 
+    pub fn set_title(&self, title: impl Into<String>) {
+        let title = title.into();
+        self.with_window(move |window| {
+            window.set_title(&title);
+        });
+    }
+
+    pub fn with_window(&self, cb: impl FnOnce(&Window) + 'static + Send + Sync) {
+        self.send(EventMessage::WithWindow(Box::new(cb))).ok();
+    }
+
     pub fn drag_window(&self) {
-        self.send(EventMessage::DragWindow).ok();
+        self.with_window(|window| {
+            window.drag_window().ok();
+        });
+    }
+
+    pub fn set_maximize_window(&self, maximize: bool) {
+        self.with_window(move |window| {
+            window.set_maximized(maximize);
+        });
+    }
+
+    pub fn toggle_maximize_window(&self) {
+        self.with_window(|window| {
+            window.set_maximized(!window.is_maximized());
+        });
+    }
+
+    pub fn set_minimize_window(&self, minimize: bool) {
+        self.with_window(move |window| {
+            window.set_minimized(minimize);
+        });
+    }
+
+    pub fn toggle_minimize_window(&self) {
+        self.with_window(|window| {
+            window.set_minimized(window.is_minimized().map(|v| !v).unwrap_or_default());
+        });
+    }
+
+    pub fn toggle_fullscreen_window(&self) {
+        self.with_window(|window| match window.fullscreen() {
+            Some(_) => window.set_fullscreen(None),
+            None => window.set_fullscreen(Some(Fullscreen::Borderless(None))),
+        });
+    }
+
+    pub fn set_fullscreen_window(&self, fullscreen: bool) {
+        self.with_window(move |window| {
+            if fullscreen {
+                window.set_fullscreen(Some(Fullscreen::Borderless(None)))
+            } else {
+                window.set_fullscreen(None)
+            }
+        });
     }
 
     pub fn request_animation_frame(&self) {
