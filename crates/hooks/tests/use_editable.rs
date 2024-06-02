7908f613bb7309ccb3172cf1f4db61cf5acb8210
marc2332 <mespinsanz@gmail.com>
2024-06-02 12:31:32 +0200

    chore: Add tests for shift + click text selection

--- crates/hooks/tests/use_editable.rs
+++ crates/hooks/tests/use_editable.rs
@@ -858,3 +858,270 @@ pub async fn backspace_remove() {
         assert_eq!(cursor.text(), Some("0:4"));
     }
 }
+
+#[tokio::test]
+pub async fn highlight_shift_click_multiple_lines_single_editor() {
+    fn use_editable_app() -> Element {
+        let mut editable = use_editable(
+            || EditableConfig::new("Hello Rustaceans\n".repeat(2)),
+            EditableMode::MultipleLinesSingleEditor,
+        );
+        let editor = editable.editor().read();
+        let cursor = editor.cursor();
+        let cursor_pos = editor.visible_cursor_pos();
+        let cursor_reference = editable.cursor_attr();
+        let highlights = editable.highlights_attr(0);
+
+        let onmousedown = move |e: MouseEvent| {
+            editable.process_event(&EditableEvent::MouseDown(e.data, 0));
+        };
+
+        let onmouseover = move |e: MouseEvent| {
+            editable.process_event(&EditableEvent::MouseOver(e.data, 0));
+        };
+
+        let onkeydown = move |e: Event<KeyboardData>| {
+            editable.process_event(&EditableEvent::KeyDown(e.data));
+        };
+
+        let onclick = move |_: MouseEvent| {
+            editable.process_event(&EditableEvent::Click);
+        };
+
+        rsx!(
+            rect {
+                width: "100%",
+                height: "100%",
+                background: "white",
+                cursor_reference,
+                paragraph {
+                    height: "50%",
+                    width: "100%",
+                    cursor_id: "0",
+                    cursor_index: "{cursor_pos}",
+                    cursor_color: "black",
+                    cursor_mode: "editable",
+                    highlights,
+                    onkeydown,
+                    onclick,
+                    onmousedown,
+                    onmouseover,
+                    text {
+                        color: "black",
+                        "{editor}"
+                    }
+                }
+                label {
+                    color: "black",
+                    height: "50%",
+                    "{cursor.row()}:{cursor.col()}"
+                }
+            }
+        )
+    }
+
+    let mut utils = launch_test(use_editable_app);
+
+    let root = utils.root().get(0);
+
+    // Click cursor
+    utils.push_event(PlatformEvent::Mouse {
+        name: EventName::MouseDown,
+        cursor: (35.0, 3.0).into(),
+        button: Some(MouseButton::Left),
+    });
+    utils.wait_for_update().await;
+    utils.push_event(PlatformEvent::Mouse {
+        name: EventName::Click,
+        cursor: (35.0, 3.0).into(),
+        button: Some(MouseButton::Left),
+    });
+    utils.wait_for_update().await;
+
+    // Press shift
+    utils.push_event(PlatformEvent::Keyboard {
+        name: EventName::KeyDown,
+        key: Key::Shift,
+        code: Code::ShiftLeft,
+        modifiers: Modifiers::default(),
+    });
+    utils.wait_for_update().await;
+
+    // Move cursor
+    utils.push_event(PlatformEvent::Mouse {
+        name: EventName::MouseDown,
+        cursor: (80.0, 20.0).into(),
+        button: Some(MouseButton::Left),
+    });
+    utils.wait_for_update().await;
+
+    // Click cursor
+    utils.push_event(PlatformEvent::Mouse {
+        name: EventName::Click,
+        cursor: (80.0, 20.0).into(),
+        button: Some(MouseButton::Left),
+    });
+    utils.wait_for_update().await;
+    utils.wait_for_update().await;
+
+    let highlights = root.child(0).unwrap().state().cursor.highlights.clone();
+
+    #[cfg(not(target_os = "linux"))]
+    let start = 5;
+    #[cfg(not(target_os = "linux"))]
+    let end = 28;
+
+    #[cfg(target_os = "linux")]
+    let start = 4;
+    #[cfg(target_os = "linux")]
+    let end = 27;
+
+    assert_eq!(highlights, Some(vec![(start, end)]))
+}
+
+#[tokio::test]
+pub async fn highlights_shift_click_single_line_mulitple_editors() {
+    fn use_editable_app() -> Element {
+        let mut editable = use_editable(
+            || EditableConfig::new("Hello Rustaceans\n".repeat(2)),
+            EditableMode::SingleLineMultipleEditors,
+        );
+        let cursor_attr = editable.cursor_attr();
+        let editor = editable.editor().read();
+
+        let onkeydown = move |e: Event<KeyboardData>| {
+            editable.process_event(&EditableEvent::KeyDown(e.data));
+        };
+
+        rsx!(
+            rect {
+                width: "100%",
+                height: "100%",
+                background: "white",
+                cursor_reference: cursor_attr,
+                onkeydown,
+                direction: "vertical",
+                {editor.lines().enumerate().map(move |(i, line)| {
+
+                    let highlights = editable.highlights_attr(i);
+
+                    let is_line_selected = editable.editor().read().cursor_row() == i;
+
+                    // Only show the cursor in the active line
+                    let character_index = if is_line_selected {
+                        editable.editor().read().visible_cursor_col().to_string()
+                    } else {
+                        "none".to_string()
+                    };
+
+                    let onmouseover = move |e: MouseEvent| {
+                        editable.process_event(&EditableEvent::MouseOver(e.data, i));
+                    };
+
+                    let onmousedown = move |e: MouseEvent| {
+                        editable.process_event(&EditableEvent::MouseDown(e.data, i));
+                    };
+
+                    let onclick = move |_: MouseEvent| {
+                        editable.process_event(&EditableEvent::Click);
+                    };
+
+                    rsx!(
+                        paragraph {
+                            width: "100%",
+                            height: "30",
+                            max_lines: "1",
+                            cursor_id: "{i}",
+                            cursor_index: "{character_index}",
+                            cursor_color: "black",
+                            cursor_mode: "editable",
+                            onclick,
+                            onmouseover,
+                            onmousedown,
+                            highlights,
+                            text {
+                                color: "black",
+                                "{line}"
+                            }
+                        }
+                    )
+                })},
+                label {
+                    color: "black",
+                    height: "50%",
+                    "{editor.cursor_row()}:{editor.cursor_col()}"
+                }
+            }
+        )
+    }
+
+    let mut utils = launch_test(use_editable_app);
+
+    let root = utils.root().get(0);
+
+    // Click cursor
+    utils.push_event(PlatformEvent::Mouse {
+        name: EventName::MouseDown,
+        cursor: (35.0, 3.0).into(),
+        button: Some(MouseButton::Left),
+    });
+    utils.wait_for_update().await;
+    utils.push_event(PlatformEvent::Mouse {
+        name: EventName::Click,
+        cursor: (35.0, 3.0).into(),
+        button: Some(MouseButton::Left),
+    });
+    utils.wait_for_update().await;
+
+    // Press shift
+    utils.push_event(PlatformEvent::Keyboard {
+        name: EventName::KeyDown,
+        key: Key::Shift,
+        code: Code::ShiftLeft,
+        modifiers: Modifiers::default(),
+    });
+
+    // Click cursor
+    utils.push_event(PlatformEvent::Mouse {
+        name: EventName::MouseDown,
+        cursor: (80.0, 35.0).into(),
+        button: Some(MouseButton::Left),
+    });
+    utils.wait_for_update().await;
+    utils.push_event(PlatformEvent::Mouse {
+        name: EventName::Click,
+        cursor: (80.0, 35.0).into(),
+        button: Some(MouseButton::Left),
+    });
+    utils.wait_for_update().await;
+
+    utils.wait_for_update().await;
+
+    let highlights_1 = root.child(0).unwrap().state().cursor.highlights.clone();
+
+    #[cfg(not(target_os = "linux"))]
+    let start = 5;
+    #[cfg(not(target_os = "linux"))]
+    let end = 17;
+
+    #[cfg(target_os = "linux")]
+    let start = 4;
+    #[cfg(target_os = "linux")]
+    let end = 17;
+
+    assert_eq!(highlights_1, Some(vec![(start, end)]));
+
+    let highlights_2 = root.child(1).unwrap().state().cursor.highlights.clone();
+
+    #[cfg(not(target_os = "linux"))]
+    let start = 0;
+    #[cfg(not(target_os = "linux"))]
+    let end = 11;
+
+    #[cfg(target_os = "linux")]
+    let start = 0;
+    #[cfg(target_os = "linux")]
+    let end = 10;
+
+    assert_eq!(highlights_2, Some(vec![(start, end)]));
+}
