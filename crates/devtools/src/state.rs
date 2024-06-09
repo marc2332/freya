6d57fc6bb94f65fc53a6502759f9900da963601e
marc2332 <mespinsanz@gmail.com>
2024-06-09 18:58:31 +0200

    feat: Tree-like explorer for devtools

--- crates/devtools/src/state.rs
+++ crates/devtools/src/state.rs
@@ -1,9 +1,13 @@
+use std::collections::HashSet;
+
 use dioxus_radio::prelude::*;
+use freya_native_core::prelude::NodeId;
 use freya_renderer::{devtools::DevtoolsReceiver, HoveredNode};
 
 pub struct DevtoolsState {
     pub(crate) hovered_node: HoveredNode,
     pub(crate) devtools_receiver: DevtoolsReceiver,
+    pub(crate) devtools_tree: HashSet<NodeId>
 }
 
 #[derive(PartialEq, Eq, Clone, Debug)]
--- crates/devtools/src/tabs/tree.rs
+++ crates/devtools/src/tabs/tree.rs
@@ -1,3 +1,5 @@
+use std::collections::HashSet;
+
 use crate::{node::NodeElement, state::DevtoolsChannel, NodeIdSerializer, Route};
 use dioxus::prelude::*;
 use dioxus_radio::prelude::use_radio;
@@ -5,6 +7,12 @@ use dioxus_router::prelude::{router, use_navigator};
 use freya_components::*;
 use freya_hooks::{theme_with, ScrollViewThemeWith};
 use freya_native_core::NodeId;
+
+#[derive(Clone, PartialEq)]
+struct NodeTreeItem {
+    is_open: Option<bool>,
+    node_id: NodeId,
+}
 
 #[allow(non_snake_case)]
 #[component]
@@ -14,28 +22,65 @@ pub fn NodesTree(
     onselected: EventHandler<NodeId>,
 ) -> Element {
     let navigator = use_navigator();
-    let radio = use_radio(DevtoolsChannel::UpdatedDOM);
+    let mut radio = use_radio(DevtoolsChannel::UpdatedDOM);
+
+    let items = {
+        let radio = radio.read();
+        let devtools_receiver = radio.devtools_receiver.borrow();
+        let mut allowed_nodes = HashSet::new();
+        devtools_receiver
+            .iter()
+            .enumerate()
+            .filter_map(|(i, node)| {
+                let node_is_allowed = i == 0
+                    || node
+                        .parent_id
+                        .map(|id| allowed_nodes.contains(&id) && radio.devtools_tree.contains(&id))
+                        .unwrap_or(true);
+                if node_is_allowed {
+                    allowed_nodes.insert(node.id);
+                    let is_open = (node.children_len != 0).then_some(radio.devtools_tree.contains(&node.id));
+                    Some(NodeTreeItem {
+                        is_open,
+                        node_id: node.id,
+                    })
+                } else {
+                    None
+                }
+            })
+            .collect::<Vec<_>>()
+    };
 
     rsx!(VirtualScrollView {
         show_scrollbar: true,
-        length: radio.read().devtools_receiver.borrow().len(),
+        length: items.len(),
         item_size: 27.0,
         theme: theme_with!(ScrollViewTheme {
             height: height.to_string().into(),
             padding: "15".into(),
         }),
-        builder_args: selected_node_id,
-        builder: move |i, selected_node_id: &Option<Option<NodeId>>| {
-            let radio = radio.read();
-            let node = radio.devtools_receiver.borrow().get(i).cloned().unwrap();
-            let node_id = node.id;
+        builder_args: (selected_node_id, items),
+        builder: move |i, options: &Option<(Option<NodeId>, Vec<NodeTreeItem>)>| {
+            let (selected_node_id, items) = options.as_ref().unwrap();
+            let item = &items[i];
+            let node_id = item.node_id;
             to_owned![onselected];
             rsx! {
                 NodeElement {
                     key: "{node_id:?}",
-                    is_selected: Some(node_id) == selected_node_id.flatten(),
-                    onselected: move |node_id: NodeId| {
+                    is_selected: Some(node_id) == *selected_node_id,
+                    is_open: item.is_open,
+                    onarrow: move |_| {
+                        let mut radio = radio.write();
+                        if radio.devtools_tree.contains(&node_id) {
+                            radio.devtools_tree.remove(&node_id);
+                        } else {
+                            radio.devtools_tree.insert(node_id);
+                        }
+                    },
+                    onselected: move |_| {
                         onselected.call(node_id);
+
                         match router().current() {
                             Route::NodeInspectorLayout { .. } => {
                                 navigator.replace(Route::NodeInspectorLayout { node_id: node_id.serialize() });
