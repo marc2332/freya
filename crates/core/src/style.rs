2536fa9cc81d13549e6b42ffd8b2c9be4a2a4a43
Marc Espin <mespinsanz@gmail.com>
2024-06-11 18:40:40 +0200

    Update crates/core/src/style.rs

--- crates/core/src/style.rs
+++ crates/core/src/style.rs
@@ -4,7 +4,7 @@ pub fn default_fonts() -> Vec<String> {
         fonts.insert(0, "Segoe UI".to_string());
         fonts.insert(1, "Segoe UI Emoji".to_string());
     } else if cfg!(target_os = "macos") {
-        fonts.insert(0, "San Francisco (SF)".to_string());
+        fonts.insert(0, ".AppleSystemUIFont".to_string());
     } else if cfg!(target_os = "linux") {
         fonts.insert(0, "Ubuntu".to_string());
     }
