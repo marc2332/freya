7b3b5fc9cc6ae24a15830475d440556158a45108
marc2332 <mespinsanz@gmail.com>
2024-07-07 19:42:23 +0200

    clean up

--- examples/counter.rs
+++ examples/counter.rs
@@ -1,30 +1,46 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
 use freya::prelude::*;
 
 fn main() {
-    launch(app);
+    launch_with_props(app, "Counter", (400.0, 350.0));
 }
 
 fn app() -> Element {
-    rsx! {
+    let mut count = use_signal(|| 0);
+
+    rsx!(
         rect {
-            width: "fill",
-            height: "fill",
+            height: "50%",
+            width: "100%",
             main_align: "center",
             cross_align: "center",
-
-            rect {
-                width: "200",
-                height: "200",
-                corner_radius: "12",
-                overflow: "clip",
-
-                // Should be clipped by a rounded rectangle
-                rect {
-                    width: "fill",
-                    height: "fill",
-                    background: "green"
-                }
+            background: "rgb(0, 119, 182)",
+            color: "white",
+            shadow: "0 4 20 5 rgb(0, 0, 0, 80)",
+            label {
+                font_size: "75",
+                font_weight: "bold",
+                "{count}"
             }
         }
-    }
+        rect {
+            height: "50%",
+            width: "100%",
+            main_align: "center",
+            cross_align: "center",
+            direction: "horizontal",
+            Button {
+                onclick: move |_| count += 1,
+                label { "Increase" }
+            }
+            Button {
+                onclick: move |_| count -= 1,
+                label { "Decrease" }
+            }
+        }
+    )
 }
