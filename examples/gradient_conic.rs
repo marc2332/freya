045777634c45ef116b572bac9828ac9723cf0eed
Savchenko Ivan <73419411+Aiving@users.noreply.github.com>
2024-07-14 06:03:19 +0000

    add example for every gradient

---
+++ examples/gradient_conic.rs
@@ -0,0 +1,19 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use freya::prelude::*;
+
+fn main() {
+    launch(app);
+}
+
+fn app() -> Element {
+    rsx!(rect {
+        height: "100%",
+        width: "100%",
+        background:
+            "conic-gradient(250deg, orange 15%, rgb(255, 0, 0) 50%, rgb(255, 192, 203) 80%)",
+    })
+}
