cf8a080a23455f101c443e9c20bc585ba6ab957a
marc2332 <mespinsanz@gmail.com>
2024-07-16 18:03:15 +0200

    update example

--- examples/gradient.rs
+++ examples/gradient.rs
@@ -9,22 +9,41 @@ fn main() {
     launch(app);
 }
 
+enum GradientExample {
+    Linear,
+    Radial,
+    Conic,
+}
+
 fn app() -> Element {
-    let mut background = use_signal(|| "linear-gradient(250deg, orange 15%, rgb(255, 0, 0) 50%, rgb(255, 192, 203) 80%)");
+    let mut gradient = use_signal(|| GradientExample::Linear);
+
+    let background = match *gradient.read() {
+        GradientExample::Linear => {
+            "linear-gradient(250deg, orange 15%, rgb(255, 0, 0) 50%, rgb(255, 192, 203) 80%)"
+        }
+        GradientExample::Radial => {
+            "radial-gradient(orange 15%, rgb(255, 0, 0) 50%, rgb(255, 192, 203) 80%)"
+        }
+        GradientExample::Conic => {
+            "conic-gradient(250deg, orange 15%, rgb(255, 0, 0) 50%, rgb(255, 192, 203) 80%)"
+        }
+    };
+
     rsx!(rect {
         height: "100%",
         width: "100%",
-        background: *background.read(),
+        background,
         Button {
-            onclick: move |_| background.set("linear-gradient(250deg, orange 15%, rgb(255, 0, 0) 50%, rgb(255, 192, 203) 80%)"),
+            onclick: move |_| gradient.set(GradientExample::Linear),
             label { "Linear Gradient" }
         }
         Button {
-            onclick: move |_| background.set("radial-gradient(orange 15%, rgb(255, 0, 0) 50%, rgb(255, 192, 203) 80%)"),
+            onclick: move |_| gradient.set(GradientExample::Radial),
             label { "Radial Gradient" }
         }
         Button {
-            onclick: move |_| background.set("conic-gradient(250deg, orange 15%, rgb(255, 0, 0) 50%, rgb(255, 192, 203) 80%)"),
+            onclick: move |_| gradient.set(GradientExample::Conic),
             label { "Conic Gradient" }
         }
     })
