1d5dfb72a2e8cb51b15a912a51e135ac3a167a87
marc2332 <mespinsanz@gmail.com>
2024-08-03 09:58:42 +0200

    feat: Allow to optionally specify width and height overrides in the generated component

--- crates/components/src/svg.rs
+++ crates/components/src/svg.rs
@@ -1,15 +1,20 @@
 #[macro_export]
 macro_rules! import_svg {
     ($component_name:ident, $path:expr, $width: expr, $height: expr) => {
+        use dioxus::prelude::component;
         // Generate a function with the name derived from the file name
         #[allow(non_snake_case)]
-        pub fn $component_name() -> freya::prelude::Element {
+        #[component]
+        pub fn $component_name(
+            #[props(default = $width.to_string())] width: String,
+            #[props(default = $height.to_string())] height: String,
+        ) -> freya::prelude::Element {
             use freya::prelude::*;
             let svg_content = include_str!($path);
 
             rsx!(svg {
-                width: $width,
-                height: $height,
+                width,
+                height,
                 svg_content
             })
         }
