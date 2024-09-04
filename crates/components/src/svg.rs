f7f214001f3bd5b8fa995f3bd06b12287e2f49b0
marc2332 <mespinsanz@gmail.com>
2024-09-04 19:10:51 +0200

    feat: Optional size for import_svg

--- crates/components/src/svg.rs
+++ crates/components/src/svg.rs
@@ -6,13 +6,14 @@
 /// # use freya::prelude::*;
 ///
 /// import_svg!(Ferris, "../../../examples/ferris.svg", "100%", "100%");
+/// import_svg!(FerrisWithRequiredSize, "../../../examples/ferris.svg");
 ///
 /// fn app() -> Element {
 ///     rsx!(Ferris {})
 /// }
 ///
 /// fn another_app() -> Element {
-///     rsx!(Ferris {
+///     rsx!(FerrisWithRequiredSize {
 ///         width: "150",
 ///         height: "40%",
 ///     })
@@ -29,12 +30,27 @@ macro_rules! import_svg {
             #[props(default = $height.to_string())] height: String,
         ) -> freya::prelude::Element {
             use freya::prelude::*;
-            let svg_content = include_str!($path);
+            let svg_data = static_bytes(include_bytes!($path));
 
             rsx!(svg {
                 width,
                 height,
-                svg_content
+                svg_data
+            })
+        }
+    };
+    ($component_name:ident, $path:expr) => {
+        // Generate a function with the name derived from the file name
+        #[allow(non_snake_case)]
+        #[dioxus::prelude::component]
+        pub fn $component_name(width: String, height: String) -> freya::prelude::Element {
+            use freya::prelude::*;
+            let svg_data = static_bytes(include_bytes!($path));
+
+            rsx!(svg {
+                width,
+                height,
+                svg_data
             })
         }
     };
