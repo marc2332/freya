f607535d857cc2f0e48dac424e71ee759e2904c8
marc2332 <mespinsanz@gmail.com>
2024-09-07 20:51:56 +0200

    chore: Revert some unnecessary changes on the examples

--- examples/counter.rs
+++ examples/counter.rs
@@ -14,19 +14,17 @@ fn app() -> Element {
 
     rsx!(
         rect {
-            rect {
-                height: "50%",
-                width: "100%",
-                main_align: "center",
-                cross_align: "center",
-                background: "rgb(0, 119, 182)",
-                color: "white",
-                shadow: "0 4 20 5 rgb(0, 0, 0, 80)",
-                label {
-                    font_size: "75",
-                    font_weight: "bold",
-                    "{count}"
-                }
+            height: "50%",
+            width: "100%",
+            main_align: "center",
+            cross_align: "center",
+            background: "rgb(0, 119, 182)",
+            color: "white",
+            shadow: "0 4 20 5 rgb(0, 0, 0, 80)",
+            label {
+                font_size: "75",
+                font_weight: "bold",
+                "{count}"
             }
         }
         rect {
