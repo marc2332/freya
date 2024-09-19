fa53a6073d87c55aa35ba6ce485d90d7d550c12a
Tropical <42101043+Tropix126@users.noreply.github.com>
2024-09-19 13:41:09 -0500

    refactor: make `a11y_role` attribute kebab case

--- crates/components/src/network_image.rs
+++ crates/components/src/network_image.rs
@@ -4,6 +4,7 @@ use freya_elements::elements as dioxus_elements;
 use freya_hooks::{
     use_applied_theme,
     use_asset_cacher,
+    use_focus,
     AssetAge,
     AssetConfiguration,
     NetworkImageTheme,
