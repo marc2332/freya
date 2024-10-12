ca8fa6bf03fbc105cfa9b2362d7fa659e765976b
marc2332 <mespinsanz@gmail.com>
2024-10-12 13:40:00 +0200

    clean up

--- examples/muda.rs
+++ examples/muda.rs
@@ -18,7 +18,7 @@ fn main() {
             .with_title("Muda")
             .with_state(menu_bar.clone())
             .with_event_loop_builder(move |event_loop_builder| {
-                let menu_bar = menu_bar.clone();
+                #[cfg(target_os = "windows")]
                 event_loop_builder.with_msg_hook(move |msg| {
                     use windows_sys::Win32::UI::WindowsAndMessaging::{
                         TranslateAcceleratorW,
@@ -40,30 +40,39 @@ fn app() -> Element {
 
     rsx!(
         WindowMenu {
-            MenuItem {
-                text: "+",
-                enabled: true,
-                onclick: move |_| count += 1
-            }
-            MenuItem {
-                text: "{count}",
-                enabled: true,
-                onclick: move |_| {}
-            }
-            MenuItem {
-                text: "-",
-                enabled: true,
-                onclick: move |_| count -= 1
-            }
-            SubMenu {
-                text: "Stuff",
-                enabled: true,
+            menu: rsx!(
                 MenuItem {
-                    text: "Reset to 0",
+                    text: "+",
                     enabled: true,
-                    onclick: move |_| count.set(0)
+                    onclick: move |_| count += 1
                 }
-            }
+                MenuItem {
+                    text: "{count}",
+                    enabled: true,
+                    onclick: move |_| {}
+                }
+                MenuItem {
+                    text: "-",
+                    enabled: true,
+                    onclick: move |_| count -= 1
+                }
+                if count() < 3 {
+                    MenuItem {
+                        text: "count is smaller than 3",
+                        enabled: true,
+                        onclick: move |_| {}
+                    }
+                }
+                SubMenu {
+                    text: "Stuff",
+                    enabled: true,
+                    MenuItem {
+                        text: "Reset to 0",
+                        enabled: true,
+                        onclick: move |_| count.set(0)
+                    }
+                }
+            )
         }
         label {
             "{count}"
@@ -72,67 +81,73 @@ fn app() -> Element {
 }
 
 #[component]
-fn WindowMenu(children: Element) -> Element {
+fn WindowMenu(menu: ReadOnlySignal<Element>) -> Element {
     let platform = use_platform();
-    let mut menus_notifier = use_context_provider(|| Signal::new(()));
 
-    let rx = use_hook(|| {
+    let click_receiver = use_hook(|| {
         let menu_bar = Arc::new(SharedMenu(consume_context::<muda::Menu>()));
         struct SharedMenu(pub muda::Menu);
 
-        unsafe impl Send for SharedMenu {};
-        unsafe impl Sync for SharedMenu {};
+        unsafe impl Send for SharedMenu {}
+        unsafe impl Sync for SharedMenu {}
 
         platform.with_window(move |window| {
             use winit::raw_window_handle::*;
+
+            #[cfg(target_os = "windows")]
             if let RawWindowHandle::Win32(handle) = window.window_handle().unwrap().as_raw() {
                 unsafe { menu_bar.0.init_for_hwnd(handle.hwnd.get()).unwrap() };
             }
+
+            #[cfg(target_os = "linux")]
+            menu.init_for_gtk_window(&window, None);
+
+            #[cfg(target_os = "macos")]
+            menu.init_for_nsapp();
         });
 
         let (tx, rx) = tokio::sync::broadcast::channel::<muda::MenuEvent>(5);
 
-        {
-            let tx = tx.clone();
-            muda::MenuEvent::set_event_handler(Some(move |event| {
-                tx.send(event);
-            }));
-        }
+        muda::MenuEvent::set_event_handler(Some(move |event| {
+            tx.send(event).ok();
+        }));
 
-        MenuReceiver(rx)
+        MenuEventReceiver(rx)
     });
 
-    let (tx, rx2) = use_hook(|| {
+    let (creation_sender, creation_receiver) = use_hook(|| {
         let (tx, rx) = tokio::sync::broadcast::channel::<()>(50);
 
-        (tx, MenuReceiver(rx))
+        (tx, MenuEventReceiver(rx))
     });
 
-    use_memo(move || {
-        menus_notifier();
+    use_effect(move || {
+        menu.read();
 
         let menu_bar = consume_context::<muda::Menu>();
 
         for item in menu_bar.items() {
             if let Some(item) = item.as_menuitem() {
-                menu_bar.remove(item);
+                menu_bar.remove(item).expect("Failed to remove menu.");
             } else if let Some(submenu) = item.as_submenu() {
-                menu_bar.remove(submenu);
+                menu_bar.remove(submenu).expect("Failed to remove submenu.");
             }
         }
 
-        tx.send(());
+        creation_sender
+            .send(())
+            .expect("Failed to notify menus of an update.");
     });
 
-    provide_context(rx);
-    provide_context(rx2);
+    provide_context(click_receiver);
+    provide_context(creation_receiver);
 
-    children
+    menu()
 }
 
-struct MenuReceiver<T>(pub Receiver<T>);
+struct MenuEventReceiver<T>(pub Receiver<T>);
 
-impl<T: Clone> Clone for MenuReceiver<T> {
+impl<T: Clone> Clone for MenuEventReceiver<T> {
     fn clone(&self) -> Self {
         Self(self.0.resubscribe())
     }
@@ -146,20 +161,11 @@ fn SubMenu(
 ) -> Element {
     let mut submenu = use_context_provider(|| Signal::new(None));
 
-    use_memo(move || {
-        let mut menus_notifier = consume_context::<Signal<()>>();
-
-        text.read();
-        enabled.read();
-
-        menus_notifier.write();
-    });
-
     use_hook(move || {
-        let mut rx = consume_context::<MenuReceiver<()>>();
+        let mut creation_receiver = consume_context::<MenuEventReceiver<()>>();
 
         spawn(async move {
-            while let Ok(ev) = rx.0.recv().await {
+            while creation_receiver.0.recv().await.is_ok() {
                 let menu_bar = consume_context::<muda::Menu>();
                 let new_submenu = muda::Submenu::new(&*text.peek(), *enabled.peek());
 
@@ -178,24 +184,14 @@ fn MenuItem(
     text: ReadOnlySignal<String>,
     enabled: ReadOnlySignal<bool>,
 ) -> Element {
-    use_memo(move || {
-        let mut menus_notifier = consume_context::<Signal<()>>();
-
-        onclick.read();
-        text.read();
-        enabled.read();
-
-        menus_notifier.write();
-    });
-
     let mut menu = use_signal(|| None);
 
     use_hook(move || {
-        let mut rx = consume_context::<MenuReceiver<()>>();
+        let mut creation_receiver = consume_context::<MenuEventReceiver<()>>();
 
         spawn(async move {
-            while let Ok(ev) = rx.0.recv().await {
-                let mut submenu_signal = try_consume_context::<Signal<Option<muda::Submenu>>>();
+            while creation_receiver.0.recv().await.is_ok() {
+                let submenu_signal = try_consume_context::<Signal<Option<muda::Submenu>>>();
 
                 let new_menu_item = muda::MenuItem::new(&*text.peek(), *enabled.peek(), None);
 
@@ -214,10 +210,10 @@ fn MenuItem(
     });
 
     use_hook(move || {
-        let mut rx = consume_context::<MenuReceiver<muda::MenuEvent>>();
+        let mut click_receiver = consume_context::<MenuEventReceiver<muda::MenuEvent>>();
 
         spawn(async move {
-            while let Ok(ev) = rx.0.recv().await {
+            while let Ok(event) = click_receiver.0.recv().await {
                 if let Some(menu) = &*menu.read() {
                     if event.id == menu.id() {
                         onclick.peek().call(());
