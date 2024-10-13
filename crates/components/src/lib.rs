d14f506bdaae6768f6af4041e7f573e8b1e57856
marc2332 <mespinsanz@gmail.com>
2024-10-13 12:22:59 +0200

    typo

--- crates/components/src/lib.rs
+++ crates/components/src/lib.rs
@@ -14,7 +14,7 @@
 /// | ![BottomTab Preview][gallery_bottom_tab] | ![NetworkImage Preview][gallery_network_image] |  ![ScrollView Preview][gallery_scroll_view] |  ![VirtualScrollView Preview][gallery_virtual_scroll_view] |
 /// | [BottomTab]        | [NetworkImage] | [ScrollView] | [VirtualScrollView] |
 /// | ![Loader Preview][gallery_loader] |  ![Menu Preview][gallery_menu] |  |  |
-/// | [Loader]        | [Loader] |  |  |
+/// | [Loader]        | [Menu] |  |  |
 #[cfg_attr(feature = "docs",
     doc = embed_doc_image::embed_image!("gallery_button", "images/gallery_button.png"),
     doc = embed_doc_image::embed_image!("gallery_filled_button", "images/gallery_filled_button.png"),
