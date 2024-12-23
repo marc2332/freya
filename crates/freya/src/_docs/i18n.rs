5c25de66a5f98f02f62606c52529527ef003a56c
Marc Espin <mespinsanz@gmail.com>
2024-12-23 17:53:33 +0100

    feat: Prepare New Docs (#788)
    * feat: New book
    
    * more
    
    * more
    
    * chore: wip async
    
    * comps, hooks and  animation, wip
    
    * router docs
    
    * chore: Some notes about hooks and components
    
    * chore: i18n and themes
    
    * more
    
    * clean up
    
    * more docs
    
    * clean up
    
    * more
    
    * tweaks
    
    * more docs
    
    * more docs
    
    * clean up
    
    * text input guide
    
    * typo 'dynamically'
    
    * rephrase native_router.md
    
    * start moving book to rustdoc
    
    * improvements
    
    * revert book
    
    * clean up
    
    * clean up
    
    * clean up
    
    * clean up
    
    * clean up
    
    * clean up
    
    * clean up
    
    * clean up
    
    * clean up
    
    * clean up
    
    * clean up
    
    * fixes
    
    * clean up
    
    * add dioxus i18n to workspace crate

---
+++ crates/freya/src/_docs/i18n.rs
@@ -0,0 +1,83 @@
+//! # i18n
+//!
+//! You may add i18n (localization) support to your Freya app by using the [**dioxus-i18n**](https://github.com/dioxus-community/dioxus-i18n) crate.
+//!
+//! ```fluent
+//! # en-US.ftl
+//!
+//! hello_world = Hello, World!
+//! hello = Hello, {$name}!
+//! ```
+//!
+//!
+//! ```fluent
+//! # es-ES.ftl
+//!
+//! hello_world = Hola, Mundo!
+//! hello = Hola, {$name}!
+//! ```
+//!
+//! ```rust
+//! # use freya::prelude::*;
+//! # use dioxus_i18n::{prelude::*, t};
+//! # use dioxus_i18n::unic_langid::langid;
+//!
+//! // main.rs
+//!
+//! fn main() {
+//!     # return;
+//!     launch(app);
+//! }
+//!
+//! #[allow(non_snake_case)]
+//! fn Body() -> Element {
+//!     // Access to the i18n state
+//!     let mut i18n = i18n();
+//!
+//!     // Update the current language
+//!     let change_to_english = move |_| i18n.set_language(langid!("en-US"));
+//!     let change_to_spanish = move |_| i18n.set_language(langid!("es-ES"));
+//!
+//!     rsx!(
+//!         rect {
+//!             rect {
+//!                 direction: "horizontal",
+//!                 Button {
+//!                     onclick: change_to_english,
+//!                     label {
+//!                         "English"
+//!                     }
+//!                 }
+//!                 Button {
+//!                     onclick: change_to_spanish,
+//!                     label {
+//!                         "Spanish"
+//!                     }
+//!                 }
+//!             }
+//!
+//!             // Get and subscribe to these messages
+//!             label { { t!("hello_world") } }
+//!             label { { t!("hello", name: "Dioxus") } }
+//!         }
+//!     )
+//! }
+//!
+//! fn app() -> Element {
+//!     // Initialize our i18n config
+//!     use_init_i18n(|| {
+//!         # return I18nConfig::new(langid!("en-US"));
+//!         I18nConfig::new(langid!("en-US"))
+//!             .with_locale(Locale::new_static(
+//!                 langid!("en-US"),
+//!                 include_str!("./en-US.ftl"),
+//!             ))
+//!             .with_locale(Locale::new_dynamic(
+//!                 langid!("es-ES"),
+//!                 "./es-ES.ftl",
+//!             ))
+//!     });
+//!
+//!     rsx!(Body {})
+//! }
+//! ```
