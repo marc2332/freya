648469535826427b38342c76c5e4902117dd7984
marc2332 <mespinsanz@gmail.com>
2024-12-23 23:09:15 +0100

    debug

--- crates/components/src/overflowed_content.rs
+++ crates/components/src/overflowed_content.rs
@@ -132,6 +132,9 @@ mod test {
         utils.wait_for_update().await;
         utils.wait_for_update().await;
         utils.wait_for_update().await;
+        #[cfg(target = linux)]
+        assert_eq!(label.layout().unwrap().area.min_x(),-30.);
+
         assert!(label.layout().unwrap().area.min_x() < -30.);
 
         sleep(Duration::from_millis(50)).await;
