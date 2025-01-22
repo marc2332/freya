d10f70019e3167176f2c3c19c3a1bbe94db3df42
Marc Espin <mespinsanz@gmail.com>
2025-01-22 18:22:49 +0100

    feat: `onvalidate` function for `Input` (#1061)
    * feat: `onvalidate` function for `Input`
    
    * clean up
    
    * clean up
    
    * chore: Add text() comment

--- examples/input.rs
+++ examples/input.rs
@@ -24,7 +24,7 @@ fn app() -> Element {
                 "Your name:"
             }
             Input {
-                value: values.read().0.clone(),
+                value: values().0,
                 placeholder: "Name",
                 onchange: move |txt| {
                     values.write().0 = txt;
@@ -35,8 +35,11 @@ fn app() -> Element {
                 "Your age:"
             }
             Input {
-                value: values.read().1.clone(),
+                value: values().1,
                 placeholder: "Age",
+                onvalidate: |validator: InputValidator| {
+                    validator.set_valid(validator.text().parse::<u8>().is_err())
+                },
                 onchange: move |txt| {
                     values.write().1 = txt;
                 }
