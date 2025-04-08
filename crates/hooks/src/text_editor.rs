ff85c2363af23c272e9d33a4129719a073d0af2d
marc2332 <mespinsanz@gmail.com>
2025-04-08 15:07:07 +0200

    hotfix: Use line length - 1 when moving the cursor to the end of a line shorter than where the cursor currently is, now also when moving the cursor up

--- crates/hooks/src/text_editor.rs
+++ crates/hooks/src/text_editor.rs
@@ -188,7 +188,7 @@ pub trait TextEditor {
                 let new_row = old_row - 1;
                 let new_row_char = self.line_to_char(new_row);
                 let new_row_len = self.line(new_row).unwrap().utf16_len();
-                let new_col = old_col.min(new_row_len);
+                let new_col = old_col.min(new_row_len - 1);
                 self.cursor_mut().set(new_row_char + new_col);
             }
 
