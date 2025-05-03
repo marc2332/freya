590bdfb26cf8a206241ed1d38e964c3c40dfabb3
RobertasJ <robertasj8@gmail.com>
2025-05-03 12:51:11 +0200

    add dev env flake

---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "A development environment for compiling anything using Freya";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      with pkgs;
+      {
+        devShells.default = pkgs.mkShell rec {
+          packages = [
+            openssl
+            pkg-config
+            fontconfig
+            python3
+          ];
+          buildInputs = [
+            libxkbcommon
+            libGL
+
+            # WINIT_UNIX_BACKEND=wayland
+            wayland
+
+            # WINIT_UNIX_BACKEND=x11
+            xorg.libXcursor
+            xorg.libXrandr
+            xorg.libXi
+            xorg.libX11
+          ];
+          LD_LIBRARY_PATH = "${lib.makeLibraryPath buildInputs}";
+        };
+      }
+    );
+}
