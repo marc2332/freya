1c6da49c6e690c1142c68e0497e576de72e73780
Marc Espin <mespinsanz@gmail.com>
2025-05-21 22:44:26 +0200

    fix: Margin should not be applied again after auto measurement (#1174)
    * fix: Margin should not be applied again after auto measurement
    
    * fmt
    
    * only reapply min max if inner sized

--- crates/torin/src/measure.rs
+++ crates/torin/src/measure.rs
@@ -266,28 +266,34 @@ where
                     true,
                 );
 
-                area.size.width = node.width.min_max(
-                    area.size.width,
-                    parent_area.size.width,
-                    available_parent_area.size.width,
-                    node.margin.left(),
-                    node.margin.horizontal(),
-                    &node.minimum_width,
-                    &node.maximum_width,
-                    self.layout_metadata.root_area.width(),
-                    phase,
-                );
-                area.size.height = node.height.min_max(
-                    area.size.height,
-                    parent_area.size.height,
-                    available_parent_area.size.height,
-                    node.margin.top(),
-                    node.margin.vertical(),
-                    &node.minimum_height,
-                    &node.maximum_height,
-                    self.layout_metadata.root_area.height(),
-                    phase,
-                );
+                // Re apply min max values after measurin with inner sized
+                // Margins are set to 0 because area.size already contains the margins
+                if node.width.inner_sized() {
+                    area.size.width = node.width.min_max(
+                        area.size.width,
+                        parent_area.size.width,
+                        available_parent_area.size.width,
+                        0.,
+                        0.,
+                        &node.minimum_width,
+                        &node.maximum_width,
+                        self.layout_metadata.root_area.width(),
+                        phase,
+                    );
+                }
+                if node.height.inner_sized() {
+                    area.size.height = node.height.min_max(
+                        area.size.height,
+                        parent_area.size.height,
+                        available_parent_area.size.height,
+                        0.,
+                        0.,
+                        &node.minimum_height,
+                        &node.maximum_height,
+                        self.layout_metadata.root_area.height(),
+                        phase,
+                    );
+                }
             }
 
             inner_sizes.width += node.padding.horizontal();
