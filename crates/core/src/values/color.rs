ef282d350b9f794ac7075a8cada3037a13df6e68
kyteidev <89528285+kyteidev@users.noreply.github.com>
2025-05-29 21:42:27 +0300

    feat: support 3 character hex colors

--- crates/core/src/values/color.rs
+++ crates/core/src/values/color.rs
@@ -193,7 +193,12 @@ fn parse_hsl(color: &str) -> Result<Color, ParseError> {
 }
 
 fn parse_hex_color(color: &str) -> Result<Color, ParseError> {
-    if color.len() == 7 {
+    if color.len() == 4 {
+        let r = u8::from_str_radix(&color[1..2].repeat(2), 16).map_err(|_| ParseError)?;
+        let g = u8::from_str_radix(&color[2..3].repeat(2), 16).map_err(|_| ParseError)?;
+        let b = u8::from_str_radix(&color[3..4].repeat(2), 16).map_err(|_| ParseError)?;
+        Ok(Color::from_rgb(r, g, b))
+    } else if color.len() == 7 {
         let r = u8::from_str_radix(&color[1..3], 16).map_err(|_| ParseError)?;
         let g = u8::from_str_radix(&color[3..5], 16).map_err(|_| ParseError)?;
         let b = u8::from_str_radix(&color[5..7], 16).map_err(|_| ParseError)?;
