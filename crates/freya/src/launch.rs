c28df422f095219f5d4979b438b6d739b1632191
Marc Espin <marc@mespin.me>
2025-06-21 15:21:48 +0200

    feat: Global error boundary for Freya apps (#1272)

--- crates/freya/src/launch.rs
+++ crates/freya/src/launch.rs
@@ -1,3 +1,7 @@
+use dioxus::prelude::{
+    ErrorBoundary,
+    ErrorContext,
+};
 use dioxus_core::{
     Element,
     VirtualDom,
@@ -223,6 +227,7 @@ pub fn launch_cfg<T: 'static + Clone>(app: AppComponent, config: LaunchConfig<T>
         Readable,
     };
     use freya_components::NativeContainer;
+    use freya_elements as dioxus_elements;
     use freya_winit::devtools::Devtools;
 
     #[derive(Props, Clone)]
@@ -242,23 +247,51 @@ pub fn launch_cfg<T: 'static + Clone>(app: AppComponent, config: LaunchConfig<T>
         #[allow(non_snake_case)]
         let App = props.app;
 
+        let handle_error = |e: ErrorContext| {
+            #[cfg(not(debug_assertions))]
+            panic!("{e:?}");
+
+            #[cfg(debug_assertions)]
+            for error in e.errors().iter() {
+                println!("{:?}", error);
+            }
+
+            #[cfg(debug_assertions)]
+            rsx!(
+                rect {
+                    width: "fill",
+                    height: "fill",
+                    background: "rgb(138, 0, 0)",
+                    color: "white",
+                    main_align: "center",
+                    cross_align: "center",
+                    label {
+                        "An unhandled error was thrown, check your logs."
+                    }
+                }
+            )
+        };
+
         rsx!(
             NativeContainer {
-                {
-                    #[cfg(all(feature = "devtools", debug_assertions))]
-                    rsx!(
-                        freya_devtools::DevtoolsView {
-                            highlighted_node: props.highlighted_node.unwrap(),
-                            devtools_receiver: props.devtools_receiver.unwrap(),
+                ErrorBoundary {
+                    handle_error,
+                    {
+                        #[cfg(all(feature = "devtools", debug_assertions))]
+                        rsx!(
+                            freya_devtools::DevtoolsView {
+                                highlighted_node: props.highlighted_node.unwrap(),
+                                devtools_receiver: props.devtools_receiver.unwrap(),
+                                App {}
+                            }
+                        )
+                    }
+                    {
+                        #[cfg(any(not(feature = "devtools"), not(debug_assertions)))]
+                        rsx!(
                             App {}
-                        }
-                    )
-                }
-                {
-                    #[cfg(any(not(feature = "devtools"), not(debug_assertions)))]
-                    rsx!(
-                        App {}
-                    )
+                        )
+                    }
                 }
             }
         )
