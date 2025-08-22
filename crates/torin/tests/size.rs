10a77c19259e66825af743371755db65c9da3fb4
Dexterdy <j.p.hagedoorn@proton.me>
2025-08-22 15:31:45 +0200

    wip

--- crates/torin/tests/size.rs
+++ crates/torin/tests/size.rs
@@ -842,6 +842,59 @@ pub fn fill_min_cross_align_center() {
 }
 
 #[test]
+pub fn content_fit_cross_align_center() {
+    let (mut layout, mut measurer) = test_utils();
+
+    let mut mocked_dom = TestingDOM::default();
+    let mut parent = Node::from_size_and_alignments_and_direction(
+        Size::Percentage(Length::new(100.)),
+        Size::Percentage(Length::new(100.)),
+        Alignment::Start,
+        Alignment::Center,
+        Direction::Vertical,
+    );
+    parent.content = Content::Fit;
+    mocked_dom.add(0, None, vec![1, 2], parent);
+    mocked_dom.add(
+        1,
+        Some(0),
+        vec![],
+        Node::from_size_and_direction(
+            Size::FillMinimum,
+            Size::Pixels(Length::new(100.)),
+            Direction::Vertical,
+        ),
+    );
+    mocked_dom.add(
+        2,
+        Some(0),
+        vec![],
+        Node::from_size_and_direction(
+            Size::Pixels(Length::new(100.)),
+            Size::Pixels(Length::new(100.)),
+            Direction::Vertical,
+        ),
+    );
+
+    layout.measure(
+        0,
+        Rect::new(Point2D::new(0.0, 0.0), Size2D::new(1000.0, 1000.0)),
+        &mut measurer,
+        &mut mocked_dom,
+    );
+
+    assert_eq!(
+        layout.get(1).unwrap().visible_area(),
+        Rect::new(Point2D::new(450.0, 0.0), Size2D::new(100.0, 100.0)),
+    );
+
+    assert_eq!(
+        layout.get(2).unwrap().visible_area(),
+        Rect::new(Point2D::new(450.0, 100.0), Size2D::new(100.0, 100.0)),
+    );
+}
+
+#[test]
 pub fn inner_percentage() {
     let (mut layout, mut measurer) = test_utils();
 
