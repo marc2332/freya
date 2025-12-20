0abc4923e582a3e44100026e60b2d5d540f48c63
Marc Espin <marc@mespin.me>
2025-12-20 13:49:55 +0100

    feat(core/edit): Add `EventsCombos` API that supports double and triple click, support double and triple click selection in `freya-edit` using `unicode-segmentation` and `EventsCombos`

--- examples/text_editing.rs
+++ examples/text_editing.rs
@@ -1,4 +1,5 @@
 use freya::{
+    helpers::from_fn,
     prelude::*,
     text_edit::*,
 };
@@ -8,50 +9,65 @@ fn main() {
 }
 
 fn app() -> impl IntoElement {
-    let holder = use_state(ParagraphHolder::default);
     let mut editable = use_editable(
-        || "Hello, World!".to_string(),
+        || "Hello Rustaceans\nHello World".to_string(),
         EditableConfig::new,
-        EditableMode::MultipleLinesSingleEditor,
+        EditableMode::SingleLineMultipleEditors,
     );
-    let focus = use_focus();
 
-    paragraph()
-        .a11y_id(focus.a11y_id())
-        .cursor_index(editable.editor().read().cursor_pos())
-        .highlights(
-            editable
-                .editor()
-                .read()
-                .get_selection()
-                .map(|selection| vec![selection])
-                .unwrap_or_default(),
+    let editor = editable.editor().read();
+
+    let on_global_key_down = move |e: Event<KeyboardEventData>| {
+        editable.process_event(EditableEvent::KeyDown {
+            key: &e.key,
+            modifiers: e.modifiers,
+        });
+    };
+
+    rect()
+        .font_family("NotoSans")
+        .width(Size::fill())
+        .height(Size::fill())
+        .background((255, 255, 255))
+        .on_global_key_down(on_global_key_down)
+        .children_iter(editor.lines().enumerate().map(move |(i, line)| {
+            let line = line.text.to_string();
+            from_fn((), line, move |line| {
+                let holder = use_state(ParagraphHolder::default);
+                let editor = editable.editor().read();
+
+                let is_selected = editor.cursor_row() == i;
+                let cursor_index = if is_selected {
+                    Some(editor.cursor_col())
+                } else {
+                    None
+                };
+
+                let on_mouse_down = move |e: Event<MouseEventData>| {
+                    editable.process_event(EditableEvent::Down {
+                        location: e.element_location,
+                        editor_id: i,
+                        holder: &holder.read(),
+                    });
+                };
+
+                paragraph()
+                    .holder(holder.read().clone())
+                    .width(Size::fill())
+                    .height(Size::px(30.0))
+                    .max_lines(1)
+                    .cursor_index(cursor_index)
+                    .cursor_color((0, 0, 0))
+                    .on_mouse_down(on_mouse_down)
+                    .highlights(editor.get_visible_selection(i).map(|h| vec![h]))
+                    .span(Span::new(line.clone()))
+                    .into()
+            })
+        }))
+        .child(
+            label()
+                .color((0, 0, 0))
+                .height(Size::percent(50.0))
+                .text(format!("{}:{}", editor.cursor_row(), editor.cursor_col())),
         )
-        .on_mouse_down(move |e: Event<MouseEventData>| {
-            focus.request_focus();
-            editable.process_event(EditableEvent::Down {
-                location: e.element_location,
-                editor_id: 0,
-                holder: &holder.read(),
-            });
-        })
-        .on_mouse_move(move |e: Event<MouseEventData>| {
-            editable.process_event(EditableEvent::Move {
-                location: e.element_location,
-                editor_id: 0,
-                holder: &holder.read(),
-            });
-        })
-        .on_global_mouse_up(move |_| editable.process_event(EditableEvent::Release))
-        .on_key_down(move |e: Event<KeyboardEventData>| {
-            editable.process_event(EditableEvent::KeyDown {
-                key: &e.key,
-                modifiers: e.modifiers,
-            });
-        })
-        .on_key_up(move |e: Event<KeyboardEventData>| {
-            editable.process_event(EditableEvent::KeyUp { key: &e.key });
-        })
-        .span(editable.editor().read().to_string())
-        .holder(holder.read().clone())
 }
