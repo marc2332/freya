a43e56e257fc9cc5bc4b3fcb1581996c7d4049ed
Marc Espin <marc@mespin.me>
2025-12-31 20:01:22 +0100

    chore(examples): Dont spawn a cmd window in Windows when running examples

--- examples/window_icon.rs
+++ examples/window_icon.rs
@@ -1,3 +1,8 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
 use freya::prelude::*;
 
 const ICON: &[u8] = include_bytes!("./freya_icon.png");
