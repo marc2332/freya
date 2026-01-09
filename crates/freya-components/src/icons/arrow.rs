50a4722cc05f6b592f3771e79cc68a04719c810b
Marc Espin <marc@mespin.me>
2026-01-09 21:30:34 +0100

    fix(components): Make `ArrowIcon` and `TickIcon` properly use margin in layout data

--- crates/freya-components/src/icons/arrow.rs
+++ crates/freya-components/src/icons/arrow.rs
@@ -8,7 +8,6 @@ use torin::{
 #[derive(Clone, PartialEq)]
 pub struct ArrowIcon {
     layout: LayoutData,
-    margin: Gaps,
     fill: Color,
     rotate: Option<f32>,
 }
@@ -36,7 +35,6 @@ impl ArrowIcon {
                 ..Default::default()
             }
             .into(),
-            margin: Gaps::new_all(0.),
             fill: Color::BLACK,
             rotate: None,
         }
@@ -64,6 +62,6 @@ impl Component for ArrowIcon {
             <svg viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path fill-rule="evenodd" clip-rule="evenodd" d="M7.18177 9.58579L0 2.40401L1.81823 0.585785L9 7.76756L16.1818 0.585787L18 2.40402L10.8182 9.58579L10.8185 9.58601L9.00023 11.4042L9 11.404L8.99977 11.4042L7.18154 9.58602L7.18177 9.58579Z" fill="{fill}" stroke="{fill}" stroke-width="2"/>
             </svg>
-        "#.as_bytes())).rotate(self.rotate).width(self.layout.width.clone()).height(self.layout.height.clone()).margin(self.margin).fill(self.fill)
+        "#.as_bytes())).rotate(self.rotate).width(self.layout.width.clone()).height(self.layout.height.clone()).margin(self.layout.margin.clone()).fill(self.fill)
     }
 }
