651603bbf6ce704008bf467f1ce94a88168ab602
Marc Espin <marc@mespin.me>
2026-02-09 14:43:45 +0100

    chore(animation): Add `animation` getter.

--- crates/freya/src/lib.rs
+++ crates/freya/src/lib.rs
@@ -121,7 +121,7 @@ pub mod prelude {
         },
         position::Position,
         size::Size,
-        visible_size::VisibleSize
+        visible_size::VisibleSize,
     };
 }
 pub mod elements {
