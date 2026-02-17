e54731da8bd3dee0b0e96ed9a5a31a3388c04e63
Marc Espin <marc@mespin.me>
2026-02-17 23:58:34 +0100

    chore(edit): Move `get_visible_seletion` impl to `TextEditor` trait (#1545)

--- crates/freya-edit/src/rope_editor.rs
+++ crates/freya-edit/src/rope_editor.rs
@@ -1,5 +1,4 @@
 use std::{
-    cmp::Ordering,
     fmt::Display,
     ops::Range,
 };
@@ -10,7 +9,6 @@ use ropey::{
 };
 
 use crate::{
-    EditorLine,
     TextSelection,
     editor_history::{
         EditorHistory,
@@ -180,71 +178,6 @@ impl TextEditor for RopeEditor {
         match self.selection {
             TextSelection::Cursor(_) => None,
             TextSelection::Range { from, to } => Some((from, to)),
-        }
-    }
-
-    fn get_visible_selection(&self, editor_line: EditorLine) -> Option<(usize, usize)> {
-        let (selected_from, selected_to) = match self.selection {
-            TextSelection::Cursor(_) => return None,
-            TextSelection::Range { from, to } => (from, to),
-        };
-
-        match editor_line {
-            EditorLine::Paragraph(line_index) => {
-                let selected_from_row = self.char_to_line(self.utf16_cu_to_char(selected_from));
-                let selected_to_row = self.char_to_line(self.utf16_cu_to_char(selected_to));
-
-                let editor_row_idx = self.char_to_utf16_cu(self.line_to_char(line_index));
-                let selected_from_row_idx =
-                    self.char_to_utf16_cu(self.line_to_char(selected_from_row));
-                let selected_to_row_idx = self.char_to_utf16_cu(self.line_to_char(selected_to_row));
-
-                let selected_from_col_idx = selected_from - selected_from_row_idx;
-                let selected_to_col_idx = selected_to - selected_to_row_idx;
-
-                // Between starting line and endling line
-                if (line_index > selected_from_row && line_index < selected_to_row)
-                    || (line_index < selected_from_row && line_index > selected_to_row)
-                {
-                    let len = self.line(line_index).unwrap().utf16_len();
-                    return Some((0, len));
-                }
-
-                match selected_from_row.cmp(&selected_to_row) {
-                    // Selection direction is from bottom -> top
-                    Ordering::Greater => {
-                        if selected_from_row == line_index {
-                            // Starting line
-                            Some((0, selected_from_col_idx))
-                        } else if selected_to_row == line_index {
-                            // Ending line
-                            let len = self.line(selected_to_row).unwrap().utf16_len();
-                            Some((selected_to_col_idx, len))
-                        } else {
-                            None
-                        }
-                    }
-                    // Selection direction is from top -> bottom
-                    Ordering::Less => {
-                        if selected_from_row == line_index {
-                            // Starting line
-                            let len = self.line(selected_from_row).unwrap().utf16_len();
-                            Some((selected_from_col_idx, len))
-                        } else if selected_to_row == line_index {
-                            // Ending line
-                            Some((0, selected_to_col_idx))
-                        } else {
-                            None
-                        }
-                    }
-                    Ordering::Equal if selected_from_row == line_index => {
-                        // Starting and endline line are the same
-                        Some((selected_from - editor_row_idx, selected_to - editor_row_idx))
-                    }
-                    _ => None,
-                }
-            }
-            EditorLine::SingleParagraph => Some((selected_from, selected_to)),
         }
     }
 
