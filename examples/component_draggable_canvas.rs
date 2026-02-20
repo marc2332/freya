52e85345fb550fcfe5e864d81218344342cafca5
Marc Espin <marc@mespin.me>
2026-02-20 20:46:33 +0100

    feat(components): Add `ResizableDraggable` for `DraggableCanvas`

--- examples/component_draggable_canvas.rs
+++ examples/component_draggable_canvas.rs
@@ -13,6 +13,16 @@ fn app() -> impl IntoElement {
         DraggableCanvas::new()
             .width(Size::fill())
             .height(Size::fill())
-            .child(Draggable::new().child(Button::new().child("Hello, World!"))),
+            .child(Draggable::new().child(Button::new().child("Hello, World!")))
+            .child(
+                ResizableDraggable::new((250., 300.))
+                    .initial_position((50., 50.))
+                    .child(
+                        rect()
+                            .expanded()
+                            .background(Color::from_rgb(30, 100, 200))
+                            .child("Resizable Draggable"),
+                    ),
+            ),
     )
 }
