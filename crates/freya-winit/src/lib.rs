8b3670d72286ba06bed2c6a0e5f2d099bd1795ef
SparkyTD <45818400+SparkyTD@users.noreply.github.com>
2026-03-05 09:18:06 +0100

    Remove redundant closure

--- crates/freya-winit/src/lib.rs
+++ crates/freya-winit/src/lib.rs
@@ -69,7 +69,7 @@ pub fn launch(launch_config: LaunchConfig) {
     use winit::event_loop::EventLoop;
 
     let mut event_loop_builder = launch_config.event_loop_builder
-        .unwrap_or_else(|| EventLoop::<NativeEvent>::with_user_event());
+        .unwrap_or_else(EventLoop::<NativeEvent>::with_user_event);
 
     let event_loop = event_loop_builder
         .build()
