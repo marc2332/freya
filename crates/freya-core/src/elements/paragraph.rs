b04668212f0318aabc7e2dc6b2bbb77c631cd94c
Marc Espin <marc@mespin.me>
2026-03-29 13:31:32 +0200

    fix(core): Pass font style for paragraph spans (#1720)

--- crates/freya-core/src/elements/paragraph.rs
+++ crates/freya-core/src/elements/paragraph.rs
@@ -322,6 +322,11 @@ impl ElementExt for ParagraphElement {
                         f32::from(text_style_state.font_size) * context.scale_factor as f32,
                     );
                     text_style.set_font_families(&font_families);
+                    text_style.set_font_style(FontStyle::new(
+                        text_style_state.font_weight.into(),
+                        text_style_state.font_width.into(),
+                        text_style_state.font_slant.into(),
+                    ));
                     text_style.set_decoration_type(text_style_state.text_decoration.into());
                     paragraph_builder.push_style(&text_style);
                     paragraph_builder.add_text(&span.text);
