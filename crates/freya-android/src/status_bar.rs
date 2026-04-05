414d98712e0c3b7193128fe500d7f633242a2372
Marc Espin <marc@mespin.me>
2026-04-05 19:42:29 +0200

    chore(android): Update `jni` to 0.22

--- crates/freya-android/src/status_bar.rs
+++ crates/freya-android/src/status_bar.rs
@@ -1,36 +1,44 @@
-use jni::objects::{
-    JObject,
+use jni::{
     JValue,
+    jni_sig,
+    jni_str,
+    objects::JObject,
 };
 use winit::platform::android::activity::AndroidApp;
 
 /// Set whether the Android status bar uses light appearance (dark icons for light backgrounds).
 pub fn set_status_bar_light(app: &AndroidApp, light: bool) -> Result<(), jni::errors::Error> {
-    let vm = unsafe { jni::JavaVM::from_raw(app.vm_as_ptr() as *mut _) }?;
-    let env = vm.attach_current_thread()?;
-    let activity: JObject = (app.activity_as_ptr() as *mut jni::sys::_jobject).into();
+    let vm = unsafe { jni::JavaVM::from_raw(app.vm_as_ptr() as *mut _) };
+    vm.attach_current_thread(|env| {
+        let activity = unsafe { JObject::from_raw(env, app.activity_as_ptr() as *mut _) };
 
-    let window = env
-        .call_method(activity, "getWindow", "()Landroid/view/Window;", &[])?
-        .l()?;
+        let window = env
+            .call_method(
+                &activity,
+                jni_str!("getWindow"),
+                jni_sig!("()Landroid/view/Window;"),
+                &[],
+            )?
+            .l()?;
 
-    let controller = env
-        .call_method(
-            window,
-            "getInsetsController",
-            "()Landroid/view/WindowInsetsController;",
-            &[],
-        )?
-        .l()?;
+        let controller = env
+            .call_method(
+                &window,
+                jni_str!("getInsetsController"),
+                jni_sig!("()Landroid/view/WindowInsetsController;"),
+                &[],
+            )?
+            .l()?;
 
-    // APPEARANCE_LIGHT_STATUS_BARS = 8
-    let appearance = if light { 8 } else { 0 };
-    env.call_method(
-        controller,
-        "setSystemBarsAppearance",
-        "(II)V",
-        &[JValue::Int(appearance), JValue::Int(8)],
-    )?;
+        // APPEARANCE_LIGHT_STATUS_BARS = 8
+        let appearance = if light { 8 } else { 0 };
+        env.call_method(
+            &controller,
+            jni_str!("setSystemBarsAppearance"),
+            jni_sig!("(II)V"),
+            &[JValue::Int(appearance), JValue::Int(8)],
+        )?;
 
-    Ok(())
+        Ok(())
+    })
 }
