20f39d8e860fa1c69b85bc1246029c9e8cf74e08
RobertasJ <robertasj8@gmail.com>
2026-04-13 19:43:52 +0200

    added a way to add an overlay
    needed if you want to use the overlay with extra packages

--- nix-template/flake.nix
+++ nix-template/flake.nix
@@ -1,13 +1,66 @@
 {
-  description = "A flake with the freya devshells";
-  inputs = {
-    freya.url = "github:marc2332/freya";
+  description = "A development environment for compiling anything using Freya";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.systems.url = "github:nix-systems/default";
+  inputs.flake-utils = {
+    url = "github:numtide/flake-utils";
+    inputs.systems.follows = "systems";
+  };
+  inputs.fenix = {
+    url = "github:nix-community/fenix";
+    inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs =
     {
-      freya,
+      nixpkgs,
+      flake-utils,
+      fenix,
       ...
     }:
-    freya;
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+
+        commonPackages = with pkgs; [
+          python3
+        ];
+
+        commonBuildInputs = with pkgs; [
+          libxkbcommon
+          libGL
+          udev
+          openssl
+          pkg-config
+          fontconfig
+          libgcc.lib
+          freetype
+
+          # WINIT_UNIX_BACKEND=wayland
+          wayland
+
+          # WINIT_UNIX_BACKEND=x11
+          libxcursor
+          libxrandr
+          libxi
+          libx11
+        ];
+
+        mkDevShell =
+          toolchains:
+          pkgs.mkShell rec {
+            packages = commonPackages ++ toolchains;
+            buildInputs = commonBuildInputs;
+            LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
+          };
+      in
+      {
+        devShells = {
+          default = mkDevShell [ ];
+          stable = mkDevShell [ fenix.packages.${system}.stable.toolchain ];
+          unstable = mkDevShell [ fenix.packages.${system}.default.toolchain ];
+        };
+      }
+    );
 }
