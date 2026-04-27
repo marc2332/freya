d0ea0f88119e96963fc388ac7c3c9b4dcfb94e6b
Jens Krause <47693+sectore@users.noreply.github.com>
2026-04-27 23:16:55 +0200

    chore: Use `toolchain` files as single source of truth (#1791)

--- flake.nix
+++ flake.nix
@@ -11,22 +11,22 @@
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs =
-    {
-      nixpkgs,
-      flake-utils,
-      fenix,
-      ...
-    }:
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    fenix,
+    ...
+  }:
     flake-utils.lib.eachDefaultSystem (
-      system:
-      let
+      system: let
         pkgs = nixpkgs.legacyPackages.${system};
 
         commonPackages = [
           pkgs.python3
-          pkgs.dioxus-cli
+          pkgs.just
           pkgs.taplo
+          pkgs.alejandra
+          pkgs.dioxus-cli
         ];
 
         commonBuildInputs = with pkgs; [
@@ -38,18 +38,20 @@
           fontconfig
           libgcc.lib
           freetype
-          gtk3
-          glib
           cairo
           gdk-pixbuf
-          libsoup_3
           pango
           atk
-          webkitgtk_4_1
           xdo
-          xdotool
 
           llvmPackages.bintools
+
+          # required by "webview" and "tray" `--features`
+          glib
+          gtk3
+          webkitgtk_4_1
+          libsoup_3
+          xdotool
 
           # WINIT_UNIX_BACKEND=wayland
           wayland
@@ -61,20 +63,41 @@
           libx11
         ];
 
-        mkDevShell =
-          toolchain:
+        mkDevShell = toolchain:
           pkgs.mkShell rec {
-            packages = commonPackages ++ [
-              toolchain
-            ];
+            packages =
+              commonPackages
+              ++ [
+                toolchain
+              ];
             buildInputs = commonBuildInputs;
             LD_LIBRARY_PATH = "${pkgs.lib.makeLibraryPath buildInputs}";
           };
-      in
-      {
+
+        stableToolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          # Whenever `channel` in `rust-toolchain.toml` has been updated, `sha256` needs to be updated as well: 
+          # 1. replace `sha256` with `pkgs.lib.fakeSha256`, 
+          # 2. copy the expected hash from the error
+          # 3. Update `sha256` with the new hash
+          sha256 = "sha256-zC8E38iDVJ1oPIzCqTk/Ujo9+9kx9dXq7wAwPMpkpg0=";
+        };
+
+        nightlyToolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain-nightly.toml;
+          # Whenever `channel` in `rust-toolchain-nightly.toml` has been updated, `sha256` needs to be updated as well: 
+          # 1. replace `sha256` with `pkgs.lib.fakeSha256`, 
+          # 2. copy the expected hash from the error
+          # 3. Update `sha256` with the new hash
+          # sha256 = pkgs.lib.fakeSha256;
+          sha256 = "sha256-4ot8+Fs79G1hUwlEYI6700QBLKdkLb33yzwOou1o5Yk=";
+        };
+      in {
+        formatter = pkgs.alejandra;
+
         devShells = {
-          default = mkDevShell fenix.packages.${system}.stable.toolchain;
-          unstable = mkDevShell fenix.packages.${system}.default.toolchain;
+          default = mkDevShell stableToolchain;
+          unstable = mkDevShell nightlyToolchain;
         };
       }
     );
