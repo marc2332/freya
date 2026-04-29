7a40111383b67e862a45650cab54061b8554f5ea
Marc Espin <marc@mespin.me>
2026-04-29 18:54:32 +0200

    feat(components): Custom error for `NetworkImage`

--- examples/component_image_viewer.rs
+++ examples/component_image_viewer.rs
@@ -15,6 +15,7 @@ fn app() -> impl IntoElement {
     let uri = "https://images.pexels.com/photos/842711/pexels-photo-842711.jpeg";
     let path = PathBuf::from("./examples/rust_logo.png");
     let embedded = ("rust-logo", include_bytes!("./rust_logo.png"));
+    let broken = PathBuf::from("./examples/this-file-does-not-exist.png");
 
     rect()
         .expanded()
@@ -22,9 +23,27 @@ fn app() -> impl IntoElement {
         .center()
         .child(
             ImageViewer::new(uri)
-                .width(Size::percent(33.))
+                .width(Size::percent(25.))
                 .a11y_alt("Beautiful landscape."),
         )
-        .child(ImageViewer::new(path).width(Size::percent(33.)))
-        .child(ImageViewer::new(embedded).width(Size::percent(33.)))
+        .child(ImageViewer::new(path).width(Size::percent(25.)))
+        .child(ImageViewer::new(embedded).width(Size::percent(25.)))
+        .child(
+            ImageViewer::new(broken)
+                .width(Size::percent(25.))
+                .height(Size::px(200.))
+                .error_renderer(|err: String| {
+                    rect()
+                        .padding(12.)
+                        .rounded_sm()
+                        .center()
+                        .background((40, 0, 0))
+                        .child(
+                            label()
+                                .color((255, 120, 120))
+                                .text(format!("Failed to load image:\n{err}")),
+                        )
+                        .into()
+                }),
+        )
 }
