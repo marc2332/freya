698226a446fd52af5f51672d81fdabc56f15ed5e
Marc Espin <marc@mespin.me>
2026-07-28 17:27:05 +0200

    update example

--- examples/component_virtual_scrollview_auto.rs
+++ examples/component_virtual_scrollview_auto.rs
@@ -9,25 +9,52 @@ fn main() {
 }
 
 fn app() -> impl IntoElement {
-    VirtualScrollView::new(|i, _| {
-        rect()
-            .key(i)
-            .width(Size::fill())
-            .height(Size::px(100.))
-            .padding(6.)
-            .child(
+    let mut length = use_state(|| 15usize);
+
+    rect()
+        .child(
+            rect()
+                .horizontal()
+                .cross_align(Alignment::Center)
+                .spacing(6.)
+                .padding(6.)
+                .child(
+                    Button::new()
+                        .on_press(move |_| *length.write() += 1)
+                        .child("Increase"),
+                )
+                .child(
+                    Button::new()
+                        .on_press(move |_| {
+                            let mut length = length.write();
+                            *length = length.saturating_sub(1);
+                        })
+                        .child("Decrease"),
+                )
+                .child(format!("Length: {}", length.read())),
+        )
+        .child(
+            VirtualScrollView::new(|i, _| {
                 rect()
-                    .expanded()
-                    .corner_radius(8.)
-                    .background((0, 119, 182))
-                    .main_align(Alignment::center())
-                    .cross_align(Alignment::center())
-                    .child(label().color(Color::WHITE).text(format!("Item {i}"))),
-            )
-            .into()
-    })
-    .length(30usize)
-    .item_size(100.)
-    .height(Size::fill())
-    .max_height(Size::window_percent(50.))
+                    .key(i)
+                    .width(Size::fill())
+                    .height(Size::px(100.))
+                    .padding(6.)
+                    .child(
+                        rect()
+                            .expanded()
+                            .corner_radius(8.)
+                            .background((0, 119, 182))
+                            .main_align(Alignment::center())
+                            .cross_align(Alignment::center())
+                            .child(label().color(Color::WHITE).text(format!("Item {i}"))),
+                    )
+                    .into()
+            })
+            .length(*length.read())
+            .item_size(100.)
+            .height(Size::fill())
+            .min_height(Size::px(200.))
+            .max_height(Size::window_percent(50.)),
+        )
 }
