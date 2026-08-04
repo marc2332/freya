58b8a4a458a60974c3463127c6e40fc7e4386bd7
Marc Espin <marc@mespin.me>
2026-08-04 14:51:59 +0200

    chore(core): Custom rendering ticker notifier

--- crates/freya-winit/src/renderer.rs
+++ crates/freya-winit/src/renderer.rs
@@ -880,7 +880,7 @@ impl ApplicationHandler<NativeEvent> for WinitRenderer {
                             PluginHandle::new(&self.proxy),
                         );
 
-                        app.ticker_sender.send(()).ok();
+                        app.ticker_sender.notify();
 
                         self.plugins.send(
                             PluginEvent::AfterRedraw {
