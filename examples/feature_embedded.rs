5a28a6aa1da9a8da76bc00e19cc566cdf51af7c2
Marc Espin <marc@mespin.me>
2026-09-06 23:08:53 +0200

    tidy up

--- examples/feature_embedded.rs
+++ examples/feature_embedded.rs
@@ -22,6 +22,7 @@ use futures_channel::mpsc::{
     unbounded,
 };
 use torin::prelude::{
+    Area,
     Size,
     Size2D,
 };
@@ -112,7 +113,7 @@ impl EmbeddedFreya {
         self.runner
             .run_in(|| self.tree.apply_mutations(mutations, scale_factor));
         self.tree.measure_layout(
-            self.size,
+            Area::from_size(self.size),
             &mut self.font_collection,
             &self.font_manager,
             &self.events_sender,
