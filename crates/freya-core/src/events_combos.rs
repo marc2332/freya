f27b46f89ef3d56e99c40b6bc5b5ab28e9fefac3
Marc Espin <marc@mespin.me>
2026-09-15 15:13:39 +0200

    fix: Type-aware events combos

--- crates/freya-core/src/events_combos.rs
+++ crates/freya-core/src/events_combos.rs
@@ -1,6 +1,9 @@
-use std::time::{
-    Duration,
-    Instant,
+use std::{
+    marker::PhantomData,
+    time::{
+        Duration,
+        Instant,
+    },
 };
 
 use torin::prelude::CursorPoint;
@@ -33,13 +36,27 @@ const MULTI_PRESS_ELAPSED: Duration = Duration::from_millis(500);
 /// })
 /// # }
 /// ```
-#[derive(Clone, Copy, PartialEq)]
-pub struct EventsCombos {
+pub struct EventsCombos<Gesture = ()> {
     pub(crate) last_press: State<Option<(Instant, CursorPoint, u8)>>,
     pub(crate) pressing: State<bool>,
+    marker: PhantomData<Gesture>,
 }
 
-impl EventsCombos {
+impl<Gesture> Clone for EventsCombos<Gesture> {
+    fn clone(&self) -> Self {
+        *self
+    }
+}
+
+impl<Gesture> Copy for EventsCombos<Gesture> {}
+
+impl<Gesture> PartialEq for EventsCombos<Gesture> {
+    fn eq(&self, other: &Self) -> bool {
+        self.last_press == other.last_press && self.pressing == other.pressing
+    }
+}
+
+impl<Gesture: Clone + PartialEq + 'static> EventsCombos<Gesture> {
     /// Get the app-wide combos state, creating it on first use.
     pub fn get() -> Self {
         match try_consume_root_context() {
@@ -48,8 +65,9 @@ impl EventsCombos {
                 let combos = EventsCombos {
                     last_press: State::create_in_scope(None, ScopeId::ROOT),
                     pressing: State::create_in_scope(false, ScopeId::ROOT),
+                    marker: PhantomData,
                 };
-                provide_context_for_scope_id(combos, ScopeId::ROOT);
+                provide_context_for_scope_id(combos.clone(), ScopeId::ROOT);
                 combos
             }
         }
