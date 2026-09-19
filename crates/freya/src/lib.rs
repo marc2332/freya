54049a67e80544dd1147afc7d0d23e8eea995327
Marc Espin <marc@mespin.me>
2026-09-19 22:37:34 +0200

    chore(docs): Remove debris `vulkan` feature

--- crates/freya/src/lib.rs
+++ crates/freya/src/lib.rs
@@ -102,7 +102,6 @@
 //! ## Misc features
 //! - `devtools`: Enables devtools support.
 //! - `metrics`: Reexports the metrics plugin. The plugin is auto-added in debug builds.
-//! - `vulkan`: Enables Vulkan rendering support.
 //! - `hotpath`: Enables Freya's internal usage of hotpath.
 //! - `hotreload`: Enables hot reload support via the `dx` CLI from `dioxus-cli`. See [Hot Reload](self::_docs::hot_reload).
 //! - `zoom-shortcuts`: Enables `Ctrl`/`Cmd` + `+`/`-`/`0` to zoom the app.
